bc06daeefd2506a08c6becf812ede90dc373f1b4
momoirox <majablgic505@gmail.com>
2021-11-03 22:30:28 +0100

    feat: Created Hospital Api

---
+++ Hospital/HospitalApi/Controllers/CityController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hospital.Repositories.Base;
+using Hospital.Model;
+using Hospital.Repositories;
+
+namespace HospitalApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CityController : ControllerBase
+    {
+        private readonly IUnitOfWork uow;
+
+        public CityController(IUnitOfWork uow)
+        {
+            this.uow = uow;
+        }
+        [HttpGet]
+        public IEnumerable<City> GetAll() {
+
+            var CityReadRepo = uow.GetRepository<ICityReadRepository>();
+
+            return CityReadRepo.GetAll(); //
+        }
+
+
+    }
+}
---
+++ Hospital/HospitalApi/Startup.cs
@@ -0,0 +1,83 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac;
+using Hospital.Infrastructure;
+using System.Reflection;
+using Hospital.Repositories.DbImplementation;
+using Hospital.Repositories.Base;
+using Autofac.Extensions.DependencyInjection;
+
+
+namespace HospitalApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "HospitalApi", Version = "v1" });
+            });
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new DbModule());
+            builder.RegisterModule(new RepositoryModule()
+            {
+
+                RepositoryAssemblies = new List<Assembly>()
+                {
+                    typeof (CityReadRepository).Assembly
+                },
+                Namespace = "Hospital.Repositories"
+
+
+            }); 
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
+            builder.Populate(services);
+            var container = builder.Build();
+            return new AutofacServiceProvider(container);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HospitalApi v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}

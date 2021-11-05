c792431e363b5f0258e79fb89fcc6faddf300adc
andjeladjuric <andjeladjuric@uns.ac.rs>
2021-11-05 17:47:52 +0100

    feat: created initial map of the hospital

--- WebClients/ManagerWebClient/src/app/app-routing.module.ts
+++ WebClients/ManagerWebClient/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HopsitalOverviewComponent } from './hopsital-overview/hopsital-overview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'overview', component: HopsitalOverviewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+export const routingComponents = [HopsitalOverviewComponent];
---
+++ WebClients/ManagerWebClient/src/app/hopsital-overview/hopsital-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hopsital-overview',
+  templateUrl: './hopsital-overview.component.html',
+  styleUrls: ['./hopsital-overview.component.css']
+})
+export class HopsitalOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

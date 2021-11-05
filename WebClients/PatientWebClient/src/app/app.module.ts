4998fa6195a5087e087986b2abd95ee9ca015f2d
jradman12 <jelenaradman12@uns.ac.rs>
2021-11-05 19:18:55 +0100

    feat: patient home page added

--- WebClients/PatientWebClient/src/app/app.module.ts
+++ WebClients/PatientWebClient/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,

896c39a976025256764f615d77dd5b48fc325030
MilanPodunavac <milan.podunavac@uns.ac.rs>
2021-11-08 20:02:04 +0100

    feat: Complaints list search feature By title and description

--- WebClients/IntegrationWebClient/src/app/complaints/complaints-list.component.ts
+++ WebClients/IntegrationWebClient/src/app/complaints/complaints-list.component.ts
@@ -7,16 +7,35 @@ import { ComplaintsService } from './complaints.service';
   styleUrls: ['./complaints-list.component.css']
 })
 export class ComplaintsListComponent implements OnInit {
-  complaints: any[] = [];
-
-
+  
 
   constructor(private _complaintsService: ComplaintsService) { }
+
+  complaints: any = [];
+  SearchString:string="";
+  nonFilteredComplaints:any=[];
 
   ngOnInit(): void {
 
     this._complaintsService.getComplaints()
             .subscribe(complaints => this.complaints = complaints);
+    this._complaintsService.getComplaints()
+            .subscribe(complaints => this.nonFilteredComplaints = complaints);
   }
 
+  FilterFn(){
+    var searchString = this.SearchString.trim().toLowerCase();
+    
+    if(searchString == ""){
+      this.complaints = this.nonFilteredComplaints;
+      return;
+    }
+
+    this.complaints = [];
+    for(var i = 0; i < this.nonFilteredComplaints.length; i++){
+        if(this.nonFilteredComplaints[i].title.toLowerCase().includes(searchString) || this.nonFilteredComplaints[i].description.toLowerCase().includes(searchString)){
+          this.complaints.push(this.nonFilteredComplaints[i]);
+        }
+    }
+  }
 }

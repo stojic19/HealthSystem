3dafb7b31b58404f2369cf6dd69a1428bd722465
MilanPodunavac <milan.podunavac@uns.ac.rs>
2021-11-08 17:11:45 +0100

    feat: RegisterPharamcyComponent -> RegisterPharmacyComponent (typo)

--- WebClients/IntegrationWebClient/src/app/app-routing.module.ts
+++ WebClients/IntegrationWebClient/src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddComplaintComponent } from './complaints/add-complaint.component';
 import { ComplaintDetailsComponent } from './complaints/complaint-details.component';
 import { ComplaintsListComponent } from './complaints/complaints-list.component';
-import { RegisterPharamcyComponent } from './pharmacy/register-pharamcy.component';
+import { RegisterPharmacyComponent } from './pharmacy/register-pharmacy.component';
 import { PharmacyListComponent } from './pharmacy-list/pharmacy-list.component';
 
 const routes: Routes = [
   {path: 'complaints', component: ComplaintsListComponent},
   {path: 'complaints/:id', component: ComplaintDetailsComponent},
   {path: 'complaint-add', component: AddComplaintComponent},
-  {path: 'pharmacy-register', component: RegisterPharamcyComponent },
+  {path: 'pharmacy-register', component: RegisterPharmacyComponent },
   {path: 'pharmacy-list', component: PharmacyListComponent },
   {path: '', redirectTo: 'complaints', pathMatch: 'full'},
   {path: '**', redirectTo: 'complaints', pathMatch: 'full'}

ae852b277abc6d498680896170a5d5ec1ff89b45
MilanPodunavac <milan.podunavac@uns.ac.rs>
2021-11-11 23:47:23 +0100

    feat: Changed namespaces on NuGet

--- Integration/IntegrationClassLib/Services/ComplaintService.cs
+++ Integration/IntegrationClassLib/Services/ComplaintService.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace IntegrationAPI.Services
+namespace Integration.Services
 {
     public class ComplaintService
     {
--- Integration/IntegrationClassLib/Services/PharmacyService.cs
+++ Integration/IntegrationClassLib/Services/PharmacyService.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace IntegrationAPI.Services
+namespace Integration.Services
 {
     public class PharmacyService
     {

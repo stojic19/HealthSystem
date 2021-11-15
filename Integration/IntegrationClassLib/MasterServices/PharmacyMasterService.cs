7c97b768e5883fd357c30dea155bc081c041d6ef
AleksandarRadisic <aleksandar.radisic@uns.ac.rs>
2021-11-15 21:44:42 +0100

    feat: First sprint

---
+++ Integration/IntegrationClassLib/MasterServices/PharmacyMasterService.cs
@@ -0,0 +1,68 @@
+﻿using Integration.Model;
+using Integration.Repositories;
+using Integration.Repositories.Base;
+using Integration.MasterServices;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Integration.MicroServices;
+
+namespace Integration.MasterServices
+{
+    public class PharmacyMasterService
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private CityMasterService _cityMasterService;
+        private PharmacyMicroSerivce pharmacyMicroService;
+        public PharmacyMasterService(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+            _cityMasterService = new CityMasterService(unitOfWork);
+            pharmacyMicroService = new PharmacyMicroSerivce();
+        }
+        public IEnumerable<Pharmacy> GetPharmacies()
+        {
+            var pharmacyRepo = unitOfWork.GetRepository<IPharmacyReadRepository>();
+            IEnumerable<Pharmacy> pharmacies = pharmacyRepo.GetAll().Include(x => x.City).ThenInclude(x => x.Country);
+            return pharmacies;
+        }
+        public Pharmacy GetPharmacyById(int id)
+        {
+            IEnumerable<Pharmacy> pharmacies = GetPharmacies();
+            Pharmacy pharmacy = pharmacies.FirstOrDefault(pharmacy => pharmacy.Id == id);
+            return pharmacy;
+        }
+        public void SavePharmacy(Pharmacy pharmacy)
+        {
+            City existingCity = _cityMasterService.GetCityByNameAndCountry(pharmacy.City.Name, pharmacy.City.Country.Name);
+            var countryRepo = unitOfWork.GetRepository<ICountryReadRepository>();
+            Country country = countryRepo.GetByName(pharmacy.City.Country.Name);
+            pharmacyMicroService.LinkPharmacyWithExistingEntities(pharmacy, existingCity, country);
+            var pharmacyRepo = unitOfWork.GetRepository<IPharmacyWriteRepository>();
+            pharmacyRepo.Add(pharmacy);
+        }
+
+        public Pharmacy FindPharmacyByName(string pharmacyName)
+        {
+            var pharmacyRepo = unitOfWork.GetRepository<IPharmacyReadRepository>();
+            DbSet<Pharmacy> existingPharmacies = pharmacyRepo.GetAll();
+            Pharmacy existingPharmacy = existingPharmacies.FirstOrDefault(pharmacy => pharmacy.Name.Equals(pharmacyName));
+            return existingPharmacy;
+        }
+        public Pharmacy FindPharmacyByApiKey(string apiKey)
+        {
+            var pharmacyRepo = unitOfWork.GetRepository<IPharmacyReadRepository>();
+            DbSet<Pharmacy> existingPharmacies = pharmacyRepo.GetAll();
+            Pharmacy pharmacy = existingPharmacies.FirstOrDefault(pharmacy => pharmacy.ApiKey.ToString().Equals(apiKey));
+            return pharmacy;
+        }
+        public bool isUnique(Pharmacy pharmacy)
+        {
+            var pharmacyRepo = unitOfWork.GetRepository<IPharmacyReadRepository>();
+            IEnumerable<Pharmacy> existingPharmacies = pharmacyRepo.GetAll().Include(x => x.City).ThenInclude(x => x.Country);
+            return pharmacyMicroService.isUnique(pharmacy, existingPharmacies);
+        }
+    }
+}
---
+++ WebClients/IntegrationWebClient/src/app/complaints/complaint-details.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ComplaintsService } from './complaints.service';
+
+@Component({
+  selector: 'app-complaint-details',
+  templateUrl: './complaint-details.component.html',
+  styleUrls: ['./complaint-details.component.css']
+})
+export class ComplaintDetailsComponent implements OnInit {
+  complaint: any;
+  id: number = -1;
+  description = "opis zalbe";
+
+  constructor(private _route: ActivatedRoute, private _complaintsService: ComplaintsService) { }
+
+  ngOnInit(): void {
+    let id = Number(this._route.snapshot.paramMap.get('id'));
+    this.id = id;
+
+    this._complaintsService.getComplaintById(id)
+            .subscribe(complaint => this.complaint = complaint);
+  }
+
+}

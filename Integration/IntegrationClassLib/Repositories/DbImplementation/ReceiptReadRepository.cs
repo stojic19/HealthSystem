88b1131d4dc19ea1fa33f2bb911c9282e2a59e31
AleksandarRadisic <aleksandar.radisic@uns.ac.rs>
2021-11-16 23:12:21 +0100

    feat: Medicine consumption report creation implemented Added required repositories, services and model classes

---
+++ Integration/IntegrationClassLib/Repositories/DbImplementation/ReceiptReadRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Integration.EfStructures;
+using Integration.Model;
+using Integration.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace Integration.Repositories.DbImplementation
+{
+    public class ReceiptReadRepository : ReadBaseRepository<int, Receipt>, IReceiptReadRepository
+    {
+        public ReceiptReadRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public IEnumerable<Receipt> GetReceiptLogsInTimeRange(TimeRange timeRange)
+        {
+            List<Receipt> receiptLogsInTimeRange = new List<Receipt>();
+            IEnumerable <Receipt> receipts = GetAll().Include(x => x.Medicine);
+            foreach (var receiptLog in receipts)
+                if (timeRange.startDate < receiptLog.ReceiptDate && receiptLog.ReceiptDate < timeRange.endDate)
+                    receiptLogsInTimeRange.Add(receiptLog);
+            return receiptLogsInTimeRange;
+        }
+    }
+}

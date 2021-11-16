1164a4e79255f96eb4d222a6bad562aa446c7f1f
NikolaRA5 <milosavljevic.ra5.2018@uns.ac.rs>
2021-11-17 00:07:37 +0100

    feat: finished registration of hospital in pharmacy layer

---
+++ Pharmacy/Pharmacy/Model/ComplaintResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Model
+{
+    public class ComplaintResponse
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public int ComplaintId { get; set; }
+        public Complaint Complaint { get; set; }
+    }
+}
---
+++ Pharmacy/Pharmacy/Repositories/DbImplementation/ComplaintWriteRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pharmacy.EfStructures;
+using Pharmacy.Model;
+using Pharmacy.Repositories.Base;
+
+namespace Pharmacy.Repositories.DbImplementation
+{
+    public class ComplaintWriteRepository : WriteBaseRepository<Complaint>, IComplaintWriteRepository
+    {
+        public ComplaintWriteRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}

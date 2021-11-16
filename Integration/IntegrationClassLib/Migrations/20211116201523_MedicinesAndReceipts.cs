c9085d7fd83aae974439b4bb68c78437f86e406a
AleksandarRadisic <aleksandar.radisic@uns.ac.rs>
2021-11-16 23:12:21 +0100

    Finishing rebasing

---
+++ Integration/IntegrationClassLib/Migrations/20211116201523_MedicinesAndReceipts.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Integration.Migrations
+{
+    public partial class MedicinesAndReceipts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Medicines",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medicines", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Receipts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MedicineId = table.Column<int>(type: "integer", nullable: false),
+                    ReceiptDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    AmountSpent = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Receipts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Receipts_Medicines_MedicineId",
+                        column: x => x.MedicineId,
+                        principalTable: "Medicines",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Receipts_MedicineId",
+                table: "Receipts",
+                column: "MedicineId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Receipts");
+
+            migrationBuilder.DropTable(
+                name: "Medicines");
+        }
+    }
+}
---
+++ Integration/IntegrationClassLib/Repositories/DbImplementation/MedicineWriteRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Integration.EfStructures;
+using Integration.Model;
+using Integration.Repositories.Base;
+
+namespace Integration.Repositories.DbImplementation
+{
+    public class MedicineWriteRepository : WriteBaseRepository<Medicine>, IMedicineWriteRepository
+    {
+        public MedicineWriteRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}

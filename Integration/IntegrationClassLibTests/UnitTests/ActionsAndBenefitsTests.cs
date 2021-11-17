9ddc377666310f7546488aad7bf0dff99a24b3a0
MilanPodunavac <milan.podunavac@uns.ac.rs>
2021-11-18 00:04:17 +0100

    feat: Initial tests 2

---
+++ Integration/IntegrationClassLibTests/UnitTests/ActionsAndBenefitsTests.cs
@@ -0,0 +1,36 @@
+﻿using Integration.MasterServices;
+using Integration.Model;
+using Integration.Repositories;
+using Integration.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntegrationClassLibTests
+{
+    public class ActionsAndBenefitsTests
+    {
+        [Fact]
+        public void Get_all_benefits()
+        {
+            
+        }
+
+        [Fact]
+        public void Find_benefit_by_id()
+        {
+            
+        }
+
+        public IEnumerable<Benefit> MakeBenefits()
+        {
+            
+        }
+    }
+}

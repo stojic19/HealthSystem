4a76df6c6ad36e669e4a958ad90682f5a5203f73
jelenaCleo <jelenastajic98@uns.ac.rs>
2021-11-18 13:11:31 +0100

    feat: Initalization of projects HospitalIntegrationTests and HospitalUnitTests.

---
+++ Hospital/HospitalIntegrationTests/UnitTest1.cs
@@ -0,0 +1,38 @@
+using Hospital.Model;
+using Hospital.Repositories;
+using HospitalIntegrationTests.Base;
+using Shouldly;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace HospitalIntegrationTests
+{
+    public class UnitTest1 : BaseTest
+    {
+        public UnitTest1(BaseFixture fixture) : base(fixture)
+        {
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            Context.Countries.Add(new Country()
+            {
+                Id = 1,
+                Name = "Srbija"
+            });
+
+            Context.Countries.Add(new Country()
+            {
+                Id = 2,
+                Name = "BiH"
+            });
+
+            Context.SaveChanges();
+            var countries = UoW.GetRepository<ICountryReadRepository>().GetAll();
+            countries.ShouldNotBeNull();
+            countries.Count().ShouldBe(2);
+        }
+    }
+}

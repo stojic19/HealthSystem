b463345f50623f1fbe7d35713125fc9244e9e1d8
AleksandarRadisic <aleksandar.radisic@uns.ac.rs>
2021-11-19 16:52:42 +0100

    feat: Registration and Complaint changes Code changed in order to adapt to pharmacy api # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # interactive rebase in progress; onto 1f45ead # Last command done (1 command done): # pick 2a739ae feat: Registration and Complaint changes Code changed in order to adapt to pharmacy api # Next command to do (1 remaining command): # pick 5bfc285 feat: renames # You are currently rebasing branch 'feature/integration-registration-and-complaint-refactor' on '1f45ead'. # # Changes to be committed: # deleted: Integration/IntegrationApi/Adapters/ComplaintAdapter.cs # deleted: Integration/IntegrationApi/Adapters/ComplaintResponseAdapter.cs # modified: Integration/IntegrationApi/Adapters/PharmacyAdapter.cs # modified: Integration/IntegrationApi/Controllers/ComplaintController.cs # new file: Integration/IntegrationApi/Controllers/ComplaintResponseController.cs # deleted: Integration/IntegrationApi/Controllers/PharmacyCommunicationController.cs # new file: Integration/IntegrationApi/Controllers/RegistrationController.cs # modified: Integration/IntegrationApi/DTO/ComplaintDTO.cs # modified: Integration/IntegrationApi/DTO/ComplaintResponseDTO.cs # modified: Integration/IntegrationApi/DTO/HospitalDTO.cs # modified: Integration/IntegrationApi/DTO/PharmacyDTO.cs # new file: Integration/IntegrationApi/DTO/PharmacyUrlDTO.cs #

--- Integration/IntegrationApi/Controllers/ComplaintController.cs
+++ Integration/IntegrationApi/Controllers/ComplaintController.cs
@@ -9,6 +9,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Integration;
+using Integration.DTO;
+using IntegrationAPI.Adapters;
+using RestSharp;
 
 namespace IntegrationAPI.Controllers
 {
@@ -17,10 +21,12 @@ namespace IntegrationAPI.Controllers
     public class ComplaintController : ControllerBase
     {
         private ComplaintMasterService _complaintMasterService;
+        private PharmacyMasterService _pharmacyMasterService;
 
         public ComplaintController(IUnitOfWork unitOfWork)
         {
             _complaintMasterService = new ComplaintMasterService(unitOfWork);
+            _pharmacyMasterService = new PharmacyMasterService(unitOfWork);
         }
 
         [HttpGet]
@@ -41,6 +47,42 @@ namespace IntegrationAPI.Controllers
             complaint.Pharmacy.Complaints = null;
             if (complaint.ComplaintResponse != null) complaint.ComplaintResponse.Complaint = null;
             return complaint;
+        }
+        [HttpPost]
+        public IActionResult PostComplaint(CreateComplaintDTO createComplaintDTO)
+        {
+            Pharmacy pharmacy = _pharmacyMasterService.GetPharmacyById(createComplaintDTO.PharmacyId);
+            Complaint complaint = new Complaint
+            {
+                Title = createComplaintDTO.Title,
+                Description = createComplaintDTO.Description,
+                PharmacyId = pharmacy.Id,
+                CreatedDate = DateTime.Now,
+                ManagerId = Program.ManagerId
+            };
+            _complaintMasterService.SaveComplaint(complaint);
+            ComplaintDTO complaintDTO = new ComplaintDTO
+            {
+                ApiKey = complaint.Pharmacy.ApiKey,
+                CreatedDate = complaint.CreatedDate,
+                Description = complaint.Description,
+                Title = complaint.Title,
+            };
+            IRestResponse response = SendComplaintToPharmacy(complaint, complaintDTO);
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                _complaintMasterService.DeleteComplaint(complaint);
+                return BadRequest("Pharmacy failed to receive complaint! Try again");
+            }
+            return Ok("Complaint saved and sent to pharmacy!");
+        }
+        private IRestResponse SendComplaintToPharmacy(Complaint complaint, ComplaintDTO complaintDTO)
+        {
+            RestClient client = new RestSharp.RestClient();
+            RestRequest request = new RestRequest(complaint.Pharmacy.BaseUrl + "/api/Complaint/CreateComplaint");
+            request.AddJsonBody(complaintDTO);
+            IRestResponse response = client.Post(request);
+            return response;
         }
     }
 }

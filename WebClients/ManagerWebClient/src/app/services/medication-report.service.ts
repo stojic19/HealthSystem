64e139fa99d6c3abcdc1fdafb28d61d1ef199fa2
AleksandarRadisic <aleksandar.radisic@uns.ac.rs>
2021-11-19 23:40:00 +0100

    feat: initial version of front

---
+++ WebClients/ManagerWebClient/src/app/services/medication-report.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MedicationReportService {
+  private _APIUrl = 'http://localhost:5000/api';
+  constructor(private _httpClient: HttpClient) { }
+  getReport(val:any): Observable<object>{
+    return this._httpClient.post<object>(this._APIUrl+'/Report/CreateConsumptionReport',val);
+  }
+}

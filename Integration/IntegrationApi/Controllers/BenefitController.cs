8d0309668dd11f8c91fa41b742dc57c8e0d0ace6
MilanPodunavac <milan.podunavac@uns.ac.rs>
2021-11-20 03:13:15 +0100

    feat: Test tag za nuget

--- Integration/IntegrationApi/Controllers/BenefitController.cs
+++ Integration/IntegrationApi/Controllers/BenefitController.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace IntegrationAPI.Controllers
 {
-    public class BenefitController
+    public class BenefitController : ControllerBase
     {
     }
 }

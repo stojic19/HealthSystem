fa78b0da22c87be579b150212da99d84dfb0f8bd
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2021-11-22 22:57:49 +0100

    Bugfix/medicine managment fix (#59)
    * fix: update and delete of medicine implemented
    
    * fix: fixed model and CRUD for medicines

--- Pharmacy/PharamcyApi/DTO/UpdateMedicineDTO.cs
+++ Pharmacy/PharamcyApi/DTO/UpdateMedicineDTO.cs
@@ -10,12 +10,13 @@ namespace PharmacyApi.DTO
     {
         [Required(ErrorMessage = "It is necessary to specify the name of medicine!")]
         public string Name { get; set; }
-        public List<string> SideEffects { get; set; }
-        public List<string> Reactions { get; set; }
+        public string SideEffects { get; set; }
+        public string Reactions { get; set; }
         public string Usage { get; set; }
+        [Range(0, int.MaxValue, ErrorMessage = "Only positive number allowed.")]
         public double WeightInMilligrams { get; set; }
-        public List<string> Precautions { get; set; }
-        public List<string> MedicinePotentialDangers { get; set; }
+        public string Precautions { get; set; }
+        public string MedicinePotentialDangers { get; set; }
         [Range(0, int.MaxValue, ErrorMessage = "Only positive number allowed.")]
         public int Quantity { get; set; }
     }

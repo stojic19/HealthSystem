7b6f73275228b85482ff24a47807ec506afc3182
Luka Matić <73677231+lukamatic@users.noreply.github.com>
2021-11-22 23:43:51 +0100

    Feature/pharmacy medicine procurement (#56) Hospital Integration Pharmacy README.md WebClients feat: Implemented medicine availability check  Hospital Integration Pharmacy README.md WebClients feat: Implemented executing medicine procurement. Refactored medicine availability checking  Hospital Integration Pharmacy README.md WebClients feat: Added request validation. Covered negative quantity edge case

---
+++ Pharmacy/PharamcyApi/DTO/CheckMedicineAvailabilityRequestDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmacyApi.DTO
+{
+    public class CheckMedicineAvailabilityRequestDTO
+    {
+        [Required(ErrorMessage = "It is necessary to provide the hospital API key.")]
+        public Guid ApiKey { get; set; }
+
+        [Required(ErrorMessage = "It is necessary to specify the medicine name.")]
+        public string MedicineName { get; set; }
+
+        [Required(ErrorMessage = "It is necessary to specify the manufacturer name.")]
+        public string ManufacturerName { get; set; }
+
+        [Required(ErrorMessage = "It is necessary to specify the quantity.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Only positive numbers allowed for quantity.")]
+        public int Quantity { get; set; }
+    }
+}

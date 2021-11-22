9cd8fa6f71b79c7d5eeb327f661361d6fd48ebc8
andjeladjuric <andjeladjuric@uns.ac.rs>
2021-11-22 16:17:12 +0100

    feat: fixed migrations

--- Hospital/HospitalClassLib/Migrations/20211122074409_AddedEquipmentTransferEvent.Designer.cs
+++ Hospital/HospitalClassLib/Migrations/20211122074409_AddedEquipmentTransferEvent.Designer.cs
@@ -10,7 +10,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Hospital.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20211121205214_AddedEquipmentTransferEvent")]
+    [Migration("20211122074409_AddedEquipmentTransferEvent")]
     partial class AddedEquipmentTransferEvent
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)

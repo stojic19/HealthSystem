1f4e3aa1279a0d198f0c7f8b140efd22e803e520
lukamatic <matic.ra16.2018@uns.ac.rs>
2021-11-22 15:10:16 +0100

    feat: Added request validation. Covered negative quantity edge case

--- Pharmacy/PharamcyApi/DTO/CheckMedicineAvailabilityRequestDTO.cs
+++ Pharmacy/PharamcyApi/DTO/CheckMedicineAvailabilityRequestDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,17 @@ namespace PharmacyApi.DTO
 {
     public class CheckMedicineAvailabilityRequestDTO
     {
+        [Required(ErrorMessage = "It is necessary to provide the hospital API key.")]
         public Guid ApiKey { get; set; }
+
+        [Required(ErrorMessage = "It is necessary to specify the medicine name.")]
         public string MedicineName { get; set; }
+
+        [Required(ErrorMessage = "It is necessary to specify the manufacturer name.")]
         public string ManufacturerName { get; set; }
+
+        [Required(ErrorMessage = "It is necessary to specify the quantity.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Only positive numbers allowed for quantity.")]
         public int Quantity { get; set; }
     }
 }

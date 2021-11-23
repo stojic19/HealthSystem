ac94cf2563c4b605522112c4ad8ae1f889e1b9e5
Luka Matić <73677231+lukamatic@users.noreply.github.com>
2021-11-23 02:44:00 +0100

    Feature/run unit tests (#63) Hospital Integration Pharmacy README.md WebClients feat: Fixed folder names for pharmacy test projects  Hospital Integration Pharmacy README.md WebClients feat: Created scripts for testing

---
+++ Pharmacy/PharmacyUnitTests/Base/BaseTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Pharmacy.EfStructures;
+using Pharmacy.Repositories.Base;
+using Xunit;
+
+namespace PharmacyUnitTests.Base
+{
+    [Collection("UnitTests")]
+    public abstract class BaseTest : IClassFixture<BaseFixture>
+    {
+        private readonly BaseFixture _fixture;
+
+        protected BaseTest(BaseFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        protected IUnitOfWork UoW => _fixture.UoW;
+        protected AppDbContext Context => _fixture.Context;
+
+        public static void GenericRemoveSet<T>(DbSet<T> set) where T : class
+        {
+            foreach (var item in set)
+            {
+                set.Remove(item);
+            }
+        }
+
+        protected void ClearDbContext()
+        {
+            var removeMethod = System.Reflection.MethodBase
+                .GetCurrentMethod()
+                .DeclaringType
+                .GetMethod(nameof(GenericRemoveSet));
+
+            var properties = Context
+                .GetType()
+                .GetProperties()
+                .Where(x => x.PropertyType.IsGenericType &&
+                            x.PropertyType.GetGenericTypeDefinition() == typeof(DbSet<>));
+
+            foreach (var property in properties)
+            {
+                var genericArgument = property.PropertyType.GetGenericArguments();
+                var typedRemove = removeMethod.MakeGenericMethod(genericArgument);
+                typedRemove.Invoke(null, new object[] { property.GetValue(Context) });
+            }
+
+            Context.SaveChanges();
+        }
+    }
+}
---
+++ Pharmacy/PharmacyUnitTests/ModelTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using Pharmacy.Model;
+using Pharmacy.Repositories;
+using PharmacyUnitTests.Base;
+using Shouldly;
+using Xunit;
+
+namespace PharmacyUnitTests
+{
+    public class ModelTests : BaseTest
+    {
+        public ModelTests(BaseFixture fixture) : base(fixture)
+        {
+        }
+
+        [Fact]
+        public void City_should_not_be_null()
+        {
+            ClearDbContext();
+
+            Context.Cities.Add(new City()
+            {
+                Name = "Test city",
+                Country = new Country()
+                {
+                    Name = "Test country"
+                },
+                PostalCode = 100
+            });
+            Context.SaveChanges();
+
+            var city = UoW.GetRepository<ICityReadRepository>().GetCityByName("Test city");
+            var cities = UoW.GetRepository<ICityReadRepository>().GetAll();
+            cities.Count().ShouldBe(1);
+            city.ShouldNotBeNull();
+        }
+    }
+}

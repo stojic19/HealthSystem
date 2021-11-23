7e7cd79de4268daa8adcbca3966995c9f2d7859b
bobabnoba <bozanaruljicz@uns.ac.rs>
2021-11-23 20:35:16 +0100

    fix: feedback methods using proxy settings

--- WebClients/ManagerWebClient/src/app/components/feedbacks-manager/feedbacks-manager.component.ts
+++ WebClients/ManagerWebClient/src/app/components/feedbacks-manager/feedbacks-manager.component.ts
@@ -8,44 +8,50 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   selector: 'app-feedbacks-manager',
   templateUrl: './feedbacks-manager.component.html',
   styleUrls: ['./feedbacks-manager.component.css'],
-  providers: [FeedbacksManagerService]
+  providers: [FeedbacksManagerService],
 })
 export class FeedbacksManagerComponent implements OnInit {
-
   allFeedbacks!: IFeedback[];
   waiting!: IFeedback[];
   approved: IFeedback[] = [];
-  sub! : Subscription;
-  constructor(private _feedbackService: FeedbacksManagerService, private _snackBar: MatSnackBar) {
-
-  }
+  sub!: Subscription;
+  constructor(
+    private _feedbackService: FeedbacksManagerService,
+    private _snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
     this.allFeedbacks = [];
     this.sub = this._feedbackService.getFeedback().subscribe({
-      next:  com => {this.allFeedbacks = com;}
-  })}
+      next: (com) => {
+        this.allFeedbacks = com;
+      },
+    });
+  }
 
   approveFeedback(feedback: IFeedback) {
-    this._feedbackService.approveFeedback(feedback).subscribe(editedFeedback => {
-      let index = this.allFeedbacks.indexOf(feedback);
-      this.allFeedbacks[index] = editedFeedback;
-    },
-      error => {                             
-        console.error('Error approving feedback!' + error.HttpErrorResponse)
-    });
-    this._snackBar.open("Feedback has been successfully approved.", "Dismiss");
+    this._feedbackService.approveFeedback(feedback).subscribe(
+      (editedFeedback) => {
+        let index = this.allFeedbacks.indexOf(feedback);
+        this.allFeedbacks[index] = editedFeedback;
+      },
+      (error) => {
+        console.error('Error approving feedback!' + error.HttpErrorResponse);
+      }
+    );
+    this._snackBar.open('Feedback has been successfully approved.', 'Dismiss');
   }
 
   unapproveFeedback(feedback: IFeedback) {
-    this._feedbackService.unapproveFeedback(feedback).subscribe(editedFeedback => {
-      let index = this.allFeedbacks.indexOf(feedback);
-      this.allFeedbacks[index] = editedFeedback;
-    },
-      error => {                             
-        console.error('Error approving feedback!' + error.HttpErrorResponse)
-    });
-    this._snackBar.open("Feedback has been successfully approved.", "Dismiss");
+    this._feedbackService.unapproveFeedback(feedback).subscribe(
+      (editedFeedback) => {
+        let index = this.allFeedbacks.indexOf(feedback);
+        this.allFeedbacks[index] = editedFeedback;
+      },
+      (error) => {
+        console.error('Error approving feedback!' + error.HttpErrorResponse);
+      }
+    );
+    this._snackBar.open('Feedback has been successfully approved.', 'Dismiss');
   }
-  
 }
--- WebClients/ManagerWebClient/src/app/services/feedbacks-manager.service.ts
+++ WebClients/ManagerWebClient/src/app/services/feedbacks-manager.service.ts
@@ -1,28 +1,23 @@
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { IFeedback } from "../interfaces/feedback";
-
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IFeedback } from '../interfaces/feedback';
 
 @Injectable()
 export class FeedbacksManagerService {
-
   constructor(private http: HttpClient) {}
 
-  getFeedback() : Observable<IFeedback[]>  {
-
-    return  this.http.get<IFeedback[]>('/api/Feedback');
+  getFeedback(): Observable<IFeedback[]> {
+    return this.http.get<IFeedback[]>('/api/Feedback/GetAllFeedbacks');
   }
-  
-  approveFeedback(feedback: IFeedback) : Observable<IFeedback>  {
-    var url =  '/api/Feedback/publish';
-    return this.http.put<any>(url, feedback );
- }
 
-  unapproveFeedback(feedback: IFeedback) : Observable<IFeedback>  {
-    var url =  '/api/Feedback/unpublish';
-    return this.http.put<any>(url, feedback );
+  approveFeedback(feedback: IFeedback): Observable<IFeedback> {
+    var url = '/api/Feedback/ApproveFeedback';
+    return this.http.put<any>(url, feedback);
   }
- 
+
+  unapproveFeedback(feedback: IFeedback): Observable<IFeedback> {
+    var url = '/api/Feedback/UnapproveFeedback';
+    return this.http.put<any>(url, feedback);
+  }
 }
-

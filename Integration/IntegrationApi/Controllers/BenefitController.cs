016f83137b230313f3f135cc99b13559ad3fdbe7
MilanPodunavac <milan.podunavac@uns.ac.rs>
2021-11-25 20:51:59 +0100

    feat: Made EP for getting relevant benefits

--- Integration/IntegrationApi/Controllers/BenefitController.cs
+++ Integration/IntegrationApi/Controllers/BenefitController.cs
@@ -67,6 +67,20 @@ namespace IntegrationAPI.Controllers
             return benefits;
         }
 
+        [HttpGet]
+        public IEnumerable<Benefit> GetRelevantBenefits()
+        {
+            IEnumerable<Benefit> benefits = _uow.GetRepository<IBenefitReadRepository>().GetRelevantBenefits();
+            foreach (var benefit in benefits)
+            {
+                if (benefit.Pharmacy == null)
+                {
+                    benefit.Pharmacy = _uow.GetRepository<IPharmacyReadRepository>().GetById(benefit.PharmacyId);
+                }
+            }
+            return benefits;
+        }
+
         [HttpPost, Produces("application/json")]
         public IActionResult PublishBenefit(BenefitIdDTO dto)
         {

ca2a0ea2e8568a1e489fb348396a4110a2c5c2b4
andjeladjuric <andjeladjuric@uns.ac.rs>
2021-12-01 21:49:24 +0100

    fixed scheduling equipment transfer

---
+++ WebClients/ManagerWebClient/src/app/model/time-period-view.spec.ts
@@ -0,0 +1,7 @@
+import { TimePeriodView } from './time-period-view';
+
+describe('TimePeriodView', () => {
+  it('should create an instance', () => {
+    expect(new TimePeriodView()).toBeTruthy();
+  });
+});

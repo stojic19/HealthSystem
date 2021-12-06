b55aa1b336e28b611d4db99a5b95cc5d1161d632
AleksandarRadisic <aleksandar.radisic@uns.ac.rs>
2021-12-06 19:18:36 +0100

    feat: Added PrescriptionController Adds prescription and sends it to integration api Integration test included

---
+++ Hospital/HospitalApi/Controllers/PrescriptionController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hospital.MedicalRecords.Model;
+using Hospital.MedicalRecords.Repository;
+using Hospital.SharedModel.Repository.Base;
+using HospitalApi.DTOs;
+using RestSharp;
+
+namespace HospitalApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PrescriptionController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly string _integrationBaseUrl;
+        public PrescriptionController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _integrationBaseUrl = "https:/localhost:44302/";
+        }
+
+        [HttpPost]
+        public IActionResult CreateNewPrescription(NewPrescriptionDTO newPrescriptionDto)
+        {
+            Patient patient = _unitOfWork.GetRepository<IPatientReadRepository>().GetById(newPrescriptionDto.PatientId);
+            if (patient == null)
+            {
+                ModelState.AddModelError("Patient Id", "Patient with that id does not exist");
+                return BadRequest(ModelState);
+            }
+
+            Medication medication = _unitOfWork.GetRepository<IMedicationReadRepository>()
+                .GetById(newPrescriptionDto.MedicineId);
+            if (medication == null)
+            {
+                ModelState.AddModelError("Medication Id", "Medication with that id does not exist");
+                return BadRequest(ModelState);
+            }
+
+            Prescription newPrescription = new Prescription
+            {
+                Patient = patient,
+                MedicationId = medication.Id,
+                Medication = medication,
+                EndDate = newPrescriptionDto.EndDate,
+                StartDate = newPrescriptionDto.StartDate,
+                IssuedDate = DateTime.Now
+            };
+            var writeRepo = _unitOfWork.GetRepository<IPrescriptionWriteRepository>();
+            writeRepo.Add(newPrescription);
+            RestClient restClient = new RestClient();
+            RestRequest request = new RestRequest(_integrationBaseUrl + "api/Prescription/SendToPharmacy");
+            request.AddJsonBody(new PrescriptionToIntegrationDTO
+            {
+                PatientFirstName = patient.FirstName,
+                PatientLastName = patient.LastName,
+                StartDate = newPrescription.StartDate,
+                EndDate = newPrescription.EndDate,
+                IssuedDate = newPrescription.IssuedDate
+            });
+            var response = restClient.PostAsync<IActionResult>(request);
+            return Ok();
+        }
+    }
+}
---
+++ Hospital/HospitalIntegrationTests/PrescriptionTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Hospital.MedicalRecords.Model;
+using Hospital.MedicalRecords.Repository;
+using Hospital.RoomsAndEquipment.Model;
+using Hospital.RoomsAndEquipment.Repository;
+using Hospital.SharedModel.Model;
+using Hospital.SharedModel.Model.Enumerations;
+using Hospital.SharedModel.Repository;
+using HospitalApi.DTOs;
+using HospitalIntegrationTests.Base;
+using Shouldly;
+using Xunit;
+
+namespace HospitalIntegrationTests
+{
+    public class PrescriptionTests : BaseTest
+    {
+        public PrescriptionTests(BaseFixture fixture) : base(fixture)
+        {
+        }
+
+        [Fact]
+        public async Task New_prescription_should_return_200()
+        {
+            Country country = new Country
+            {
+                Name = "PrescriptionTestCountry"
+            };
+            UoW.GetRepository<ICountryWriteRepository>().Add(country);
+            City city = new City
+            {
+                Name = "PrescriptionTestCity",
+                CountryId = country.Id
+            };
+            UoW.GetRepository<ICityWriteRepository>().Add(city);
+            Specialization specialization = new Specialization
+            {
+                Name = "PrescriptionTestSpecialization"
+            };
+            UoW.GetRepository<ISpecializationWriteRepository>().Add(specialization);
+            Room room = new Room
+            {
+                RoomType = RoomType.AppointmentRoom
+            };
+            UoW.GetRepository<IRoomWriteRepository>().Add(room);
+            Doctor doctor = new Doctor
+            {
+                FirstName = "PrescriptionTestDoctor",
+                CityId = city.Id,
+                SpecializationId = specialization.Id,
+                RoomId = room.Id
+            };
+            UoW.GetRepository<IDoctorWriteRepository>().Add(doctor);
+            MedicalRecord medRec = new MedicalRecord
+            {
+                DoctorId = doctor.Id
+            };
+            UoW.GetRepository<IMedicalRecordWriteRepository>().Add(medRec);
+            Patient patient = new Patient
+            {
+                FirstName = "PrescriptionTestPatientFirstName",
+                LastName = "PrescriptionTestLastName",
+                CityId = city.Id,
+                MedicalRecordId = medRec.Id
+            };
+            Medication medication = new Medication
+            {
+                Name = "PrescriptionTestMedication"
+            };
+            UoW.GetRepository<IPatientWriteRepository>().Add(patient);
+            UoW.GetRepository<IMedicationWriteRepository>().Add(medication);
+            var content = new NewPrescriptionDTO
+            {
+                PatientId = patient.Id,
+                MedicineId = medication.Id,
+                EndDate = new DateTime(2022, 11, 20),
+                StartDate = new DateTime(2020, 1, 3)
+            };
+            var response = await Client.PostAsync(BaseUrl + "api/Prescription/CreateNewPrescription", GetContent(content));
+            response.StatusCode.ShouldBe(HttpStatusCode.OK);
+            var presc = UoW.GetRepository<IPrescriptionReadRepository>().GetAll()
+                .FirstOrDefault(x => x.StartDate == content.StartDate);
+            UoW.GetRepository<IPrescriptionWriteRepository>().Delete(presc);
+            UoW.GetRepository<ICountryWriteRepository>().Delete(country);
+            UoW.GetRepository<IRoomWriteRepository>().Delete(room);
+            UoW.GetRepository<ISpecializationWriteRepository>().Delete(specialization);
+            UoW.GetRepository<IMedicationWriteRepository>().Delete(medication);
+        }
+    }
+}

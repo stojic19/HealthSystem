e28b8394270e1db66c57ee1d26a6fb004e614c3a
stojic19 <aleksa.stojic19@uns.ac.rs>
2021-12-08 23:01:50 +0100

    feat: Relocation of MedicationInventory service, repo and model to hospital lib

---
+++ Hospital/HospitalClassLib/MedicalRecords/Repository/Implementation/MedicationInventoryReadRepository.cs
@@ -0,0 +1,25 @@
+﻿using Hospital.Database.EfStructures;
+using Hospital.MedicalRecords.Model;
+using Hospital.SharedModel.Repository.Base;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.MedicalRecords.Repository.Implementation
+{
+    public class MedicationInventoryReadRepository : ReadBaseRepository<int, MedicationInventory>, IMedicationInventoryReadRepository
+    {
+        public MedicationInventoryReadRepository(AppDbContext context) : base(context)
+        {
+        }
+        public MedicationInventory GetMedicationByMedicationId(int id)
+        {
+            DbSet<MedicationInventory> inventory = GetAll();
+            MedicationInventory medication = inventory.FirstOrDefault(tempMedication => tempMedication.MedicationId.Equals(id));
+            return medication;
+        }
+    }
+}
---
+++ Hospital/HospitalClassLib/MedicalRecords/Repository/Implementation/MedicationInventoryWriteRepository.cs
@@ -0,0 +1,19 @@
+﻿using Hospital.Database.EfStructures;
+using Hospital.MedicalRecords.Model;
+using Hospital.SharedModel.Repository.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.MedicalRecords.Repository.Implementation
+{
+    class MedicationInventoryWriteRepository : WriteBaseRepository<MedicationInventory>, IMedicationInventoryWriteRepository
+    {
+        public MedicationInventoryWriteRepository(AppDbContext context) : base(context)
+        {
+
+        }
+    }
+}

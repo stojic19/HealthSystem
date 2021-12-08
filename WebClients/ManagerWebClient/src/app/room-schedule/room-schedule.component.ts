a44bf8f73b82ecca335a49b6da84083ca66174a1
andjeladjuric <andjeladjuric@uns.ac.rs>
2021-12-08 16:49:07 +0100

    feat: added confirmation dialog for cancelling event

--- WebClients/ManagerWebClient/src/app/room-schedule/room-schedule.component.ts
+++ WebClients/ManagerWebClient/src/app/room-schedule/room-schedule.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { EquipmentTransferEvent } from '../model/equipment-transfer-event';
 import { RoomRenovationEvent } from '../model/room-renovation-event';
 import { RoomScheduleService } from '../services/room-schedule.service';
+import {
+  ConfirmDialogComponent,
+  ConfirmDialogModel,
+} from './confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-room-schedule',
@@ -14,7 +19,8 @@ export class RoomScheduleComponent implements OnInit {
 
   constructor(
     public roomScheduleService: RoomScheduleService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    public dialog: MatDialog
   ) {
     this.route.params.subscribe((params) => {
       this.roomId = +params['id'];
@@ -25,6 +31,7 @@ export class RoomScheduleComponent implements OnInit {
   renovationEvents: RoomRenovationEvent[];
   isLoading = true;
   selectedEvent: EquipmentTransferEvent;
+  result: boolean;
 
   ngOnInit(): void {
     this.getAllEquipmentTransfers();
@@ -55,5 +62,42 @@ export class RoomScheduleComponent implements OnInit {
   getAllAppointments() {
     this.isLoading = false;
     //dodati ucitavanje termina
+  }
+
+  confirmTransferCancelDialog(): void {
+    const message = 'Are you sure you want to cancel selected transfer?';
+
+    const dialogData = new ConfirmDialogModel(
+      'Confirm Cancelling Transfer Event',
+      message
+    );
+
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      maxWidth: '600px',
+      data: dialogData,
+    });
+
+    dialogRef.afterClosed().subscribe((dialogResult) => {
+      this.result = dialogResult;
+      console.log(this.result);
+    });
+  }
+
+  confirmRenovationCancelDialog(): void {
+    const message = 'Are you sure you want to cancel selected renovation?';
+
+    const dialogData = new ConfirmDialogModel(
+      'Confirm Cancelling room renovation',
+      message
+    );
+
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      maxWidth: '600px',
+      data: dialogData,
+    });
+
+    dialogRef.afterClosed().subscribe((dialogResult) => {
+      this.result = dialogResult;
+    });
   }
 }

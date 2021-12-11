434cfeff9f9f7abb0069c7bfdbd66f0e388ac891
Jelena Radman <73402841+jradman12@users.noreply.github.com>
2021-12-11 22:14:02 +0100

    feat: activation account (#82)
    * feat: added email service,login page and sending email logic
    
    * feat: added unit test
    
    * feat: nuget version updated
    
    * feat: updated nuget version
    
    Co-authored-by: srdjansukovic <73430701+srdjansukovic@users.noreply.github.com>

--- WebClients/PatientWebClient/src/app/app-routing.module.ts
+++ WebClients/PatientWebClient/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './components/login/login.component';
 import { PatientMedicalRecordComponent } from './components/patient-medical-record/patient-medical-record.component';
 import { AppointmentsPageComponent } from './components/appointments-page/appointments-page.component';
 import { SurveyPageComponent } from './components/survey-page/survey-page.component';
@@ -41,6 +42,7 @@ const routes: Routes = [
     ],
   },
   { path: 'registration', component: RegistrationComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
--- WebClients/PatientWebClient/src/app/app.module.ts
+++ WebClients/PatientWebClient/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { FeedbacksPageComponent } from './components/feedbacks-page/feedbacks-pa
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MainComponent } from './components/main/main.component';
 import { RegistrationComponent } from './components/registration/registration.component';
+import { LoginComponent } from './components/login/login.component';
 import { PatientMedicalRecordComponent } from './components/patient-medical-record/patient-medical-record.component';
 import { AppointmentsPageComponent } from './components/appointments-page/appointments-page.component';
 import { SurveyPageComponent } from './components/survey-page/survey-page.component';
@@ -34,11 +35,9 @@ import { SurveySectionComponent } from './components/survey-section/survey-secti
     RegistrationComponent,
     PatientMedicalRecordComponent,
     AppointmentsPageComponent,
-    SurveyPageComponent,
     SurveySectionComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     NgbModule,
     BrowserAnimationsModule,

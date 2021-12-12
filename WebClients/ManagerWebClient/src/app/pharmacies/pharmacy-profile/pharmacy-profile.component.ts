d5caeba6d06f695843e93019aac82c14c8aeefe8
Jeckovic <jeckovic.ra85.2018@uns.ac.rs>
2021-12-12 22:20:05 +0100

    feat: Image upload and preview

--- WebClients/ManagerWebClient/src/app/pharmacies/pharmacy-profile/pharmacy-profile.component.ts
+++ WebClients/ManagerWebClient/src/app/pharmacies/pharmacy-profile/pharmacy-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { PharmacyService } from 'src/app/services/pharmacy.service';
 
@@ -10,17 +11,26 @@ import { PharmacyService } from 'src/app/services/pharmacy.service';
 export class PharmacyProfileComponent implements OnInit {
   pharmacy: any;
   id: number = -1;
-  imageSrc: string = "./assets/images/no-image.jpg";
+  imageSrc: SafeStyle  = "./assets/images/no-image.jpg";
   imageFile: File;
 
-  constructor(private _route: ActivatedRoute, private _pharmacyService: PharmacyService) { }
+  constructor(private _route: ActivatedRoute, private _pharmacyService: PharmacyService, private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     let id = Number(this._route.snapshot.paramMap.get('id'));
     this.id = id;
 
     this._pharmacyService.getPharmacyById(id)
-    .subscribe(pharmacies => {this.pharmacy = pharmacies},
+    .subscribe(pharmacies => {this.pharmacy = pharmacies, this.getImage()},
+      (error) => alert(error.error));
+  }
+
+  getImage(){
+    this._pharmacyService.getImage(this.imageReq())
+    .subscribe(image => {
+      if(image != "err"){
+        this.imageSrc = this.sanitizer.bypassSecurityTrustResourceUrl('data:image/jpg;base64,' + image);
+      }},
       (error) => alert(error.error));
   }
 
@@ -36,25 +46,38 @@ export class PharmacyProfileComponent implements OnInit {
   }
 
   UpdatePharmacy(){
-    this._pharmacyService.updatePharmacy(this.MakeRequest())
+    this._pharmacyService.updatePharmacy(this.makeRequest())
     .subscribe(res => {},
+      (error) => alert(error.error)
+      );
+
+    const formData = new FormData();
+    formData.append(this.imageFile.name, this.imageFile);
+    this._pharmacyService.uploadImage(formData).subscribe(res => {},
       (error) => alert(error.error)
       );
   }
 
-  MakeRequest(){
-    var updatedPharmacy = {           //napraviti DTO na backu
-      PharmacyId : this.pharmacy.id,
+  makeRequest(){
+    var updatedPharmacy = {
+      Id : this.id,
       PharmacyName: this.pharmacy.name,
-      StreetNumber: this.pharmacy.streetNumber,
-      StreetName: this.pharmacy.streetName,
       CityName: this.pharmacy.city.name,
       PostalCode: this.pharmacy.city.postalCode,
-      Country: this.pharmacy.city.country.name,
+      StreetName: this.pharmacy.streetName,
+      StreetNumber: this.pharmacy.streetNumber,
+      CountryName: this.pharmacy.city.country.name,
       Description: this.pharmacy.description,
-      Image: this.imageFile
+      ImageName: this.imageFile.name
     }
     return updatedPharmacy;
+  }
+
+  imageReq(){
+    var imageReq = {
+      ImageName: this.pharmacy.imageName
+    }
+    return imageReq;
   }
 
 }

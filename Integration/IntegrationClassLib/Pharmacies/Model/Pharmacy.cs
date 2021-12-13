0a94fd6b51a44623829e7716e65cc4f8bd302a16
Jeckovic <jeckovic.ra85.2018@uns.ac.rs>
2021-12-13 12:40:20 +0100

    feat: Added ImageName and Description in database

--- Integration/IntegrationClassLib/Pharmacies/Model/Pharmacy.cs
+++ Integration/IntegrationClassLib/Pharmacies/Model/Pharmacy.cs
@@ -21,6 +21,10 @@ namespace Integration.Pharmacies.Model
 
         public string BaseUrl { get; set; }
 
+        public string Description { get; set; }
+
+        public string ImageName { get; set; }
+
         public bool isEqual(Pharmacy pharmacy)
         {
             return Name.Equals(pharmacy.Name) && StreetName.Equals(pharmacy.StreetName) && StreetNumber.Equals(pharmacy.StreetNumber)

be02eabab46914a11ab29313d0c36f808202cc36
bobabnoba <bozanaruljicz@uns.ac.rs>
2021-12-14 00:53:51 +0100

    feat: scheduling finished

--- WebClients/PatientWebClient/src/app/app-routing.module.ts
+++ WebClients/PatientWebClient/src/app/app-routing.module.ts
@@ -10,15 +10,12 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-<<<<<<< HEAD
 import { BasicAppointmentComponent } from './components/basic-appointment/basic-appointment.component';
 import { MedicalRecordComponent } from './components/medical-record/medical-record.component';
-=======
 import { LoginComponent } from './components/login/login.component';
 import { PatientMedicalRecordComponent } from './components/patient-medical-record/patient-medical-record.component';
 import { AppointmentsPageComponent } from './components/appointments-page/appointments-page.component';
 import { SurveyPageComponent } from './components/survey-page/survey-page.component';
->>>>>>> development
 
 const routes: Routes = [
   {
@@ -37,12 +34,10 @@ const routes: Routes = [
         path: 'record',
         component: PatientMedicalRecordComponent,
       },
-        {path:'appointments',
-        component: AppointmentsPageComponent
-      },
+      { path: 'appointments', component: BasicAppointmentComponent },
       {
-        path:'surveys',
-        component: SurveyPageComponent
+        path: 'surveys',
+        component: SurveyPageComponent,
       },
       {
         path: 'medicalRecord',
@@ -64,6 +59,10 @@ const routes: Routes = [
     HttpClientModule,
   ],
   exports: [RouterModule, MaterialModule, FormsModule],
-  entryComponents: [FeedbackComponent, AppointmentsPageComponent, SurveyPageComponent],
+  entryComponents: [
+    FeedbackComponent,
+    AppointmentsPageComponent,
+    SurveyPageComponent,
+  ],
 })
 export class AppRoutingModule {}

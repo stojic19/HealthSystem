a7e3bd60f3644b0c65d8a04984e19370fa36deee
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2021-12-13 17:33:20 +0100

    Feature/pharmacy tendering (#94)
    * feat: create TenderOffer implemented
    
    * feat: server port changed
    
    * feat: Confirm tenderOffer implemented
    
    * feat: Applying to tender implemented
    
    * feat: Added price to create/update medicine
    
    * feat: added tenderId for easier communication

--- Pharmacy/Pharmacy/Migrations/20211212220556_TenderOffersAdded.Designer.cs
+++ Pharmacy/Pharmacy/Migrations/20211212220556_TenderOffersAdded.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Pharmacy.EfStructures;
@@ -9,9 +10,10 @@ using Pharmacy.EfStructures;
 namespace Pharmacy.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211212220556_TenderOffersAdded")]
+    partial class TenderOffersAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -210,9 +212,6 @@ namespace Pharmacy.Migrations
 
                     b.Property<string>("Precautions")
                         .HasColumnType("text");
-
-                    b.Property<double>("Price")
-                        .HasColumnType("double precision");
 
                     b.Property<int>("Quantity")
                         .HasColumnType("integer");

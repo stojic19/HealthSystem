53bf831a0d1abe8da81a2f471b754aa255266bc9
momoirox <majablgic505@gmail.com>
2021-12-21 21:57:09 +0100

    fix: Fixed background service

--- Hospital/HospitalClassLib/Schedule/Service/ConsumeScopedServiceHostedService.cs
+++ Hospital/HospitalClassLib/Schedule/Service/ConsumeScopedServiceHostedService.cs
@@ -1,6 +1,9 @@
-﻿using Hospital.Schedule.Service.Interfaces;
+﻿using Hospital.Schedule.Repository;
+using Hospital.Schedule.Service.Interfaces;
 using Hospital.SharedModel.Repository.Base;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,28 +11,31 @@ namespace Hospital.Schedule.Service
 {
     public class ConsumeScopedServiceHostedService : BackgroundService
     {
-        private readonly IUnitOfWork uow;
+        private readonly IUnitOfWork _uow;
         private IScheduledEventService scheduledEventsService;
+
         public ConsumeScopedServiceHostedService(IUnitOfWork unitOfWork, IScheduledEventService scheduledEventsService)
         {
-            this.uow = unitOfWork;
-            this.scheduledEventsService = scheduledEventsService;       
+            this._uow = unitOfWork;
+            this.scheduledEventsService = scheduledEventsService;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            await UpdateFinishedUserEvents(stoppingToken);
+            
+            await DoWork(stoppingToken);
         }
 
-        private async Task UpdateFinishedUserEvents(CancellationToken cancellationToken)
+        private async Task DoWork(CancellationToken stoppingToken)
         {
-            System.Diagnostics.Debug.WriteLine("whatevers");
-            while (!cancellationToken.IsCancellationRequested)
+            while (!stoppingToken.IsCancellationRequested)
             {
-                
-                scheduledEventsService.UpdateFinishedUserEvents();
-                await Task.Delay(60000);
+               System.Diagnostics.Debug.WriteLine("-- background service running --");
+
+                scheduledEventsService.UpdateFinishedUserEvents();           
+                await Task.Delay(60000, stoppingToken);
             }
         }
+      
     }
 }

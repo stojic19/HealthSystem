5321b2e7897bb6969aa2a556a5fc1ec37e7004e1
jradman12 <jelenaradman12@uns.ac.rs>
2021-12-23 12:32:19 +0100

    feat: publish feedback selenium test added

---
+++ Hospital/SeleniumTests/ApproveFeedbackTests.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using Xunit;
+
+namespace SeleniumTests
+{
+    public class ApproveFeedbackTests : IDisposable
+    {
+        private readonly IWebDriver driver;
+        private Pages.ApproveFeedbackPage approveFeedbackPage;
+
+        public ApproveFeedbackTests() {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("start-maximized");            
+            options.AddArguments("disable-infobars");           
+            options.AddArguments("--disable-extensions");       
+            options.AddArguments("--disable-gpu");              
+            options.AddArguments("--disable-dev-shm-usage");    
+            options.AddArguments("--no-sandbox");               
+            options.AddArguments("--disable-notifications");    
+            driver = new ChromeDriver(options);
+            approveFeedbackPage = new Pages.ApproveFeedbackPage(driver);      
+            approveFeedbackPage.Navigate();
+            approveFeedbackPage.EnsurePageIsDisplayed();
+        }
+        [Fact]
+        public void ApproveFeedback()
+        {
+            approveFeedbackPage.Approve();
+            Assert.True(approveFeedbackPage.UnapproveButtonDisplayed()); 
+        }
+        public void Dispose()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+    }
+}
---
+++ Hospital/SeleniumTests/Pages/ApproveFeedbackPage.cs
@@ -0,0 +1,74 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+
+namespace SeleniumTests.Pages
+{
+    public class ApproveFeedbackPage
+    {
+        private readonly IWebDriver driver;
+        private readonly string URI= "http://localhost:4200/feedbacks";
+        private IWebElement ApproveButton => driver.FindElement(By.Name("approve"));
+
+        private IWebElement UnapproveButton => driver.FindElement(By.Name("unapprove"));
+
+        public ApproveFeedbackPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+        public void Approve()
+        {
+           ApproveButton.Click();
+           EnsureUnapproveButtonIsDisplayed();
+        }
+        public void Navigate() => driver.Navigate().GoToUrl(URI);
+        public bool ApproveButtonDisplayed()
+        {
+            return ApproveButton.Displayed;
+        }
+        public bool UnapproveButtonDisplayed()
+        {
+            return UnapproveButton.Displayed;
+        }
+        public void EnsurePageIsDisplayed()
+        {
+            var wait = new WebDriverWait(driver, new TimeSpan(0, 0, 30));
+            wait.Until(condition =>
+            {
+                try
+                {
+                    return ApproveButton.Displayed;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+        }
+        public void EnsureUnapproveButtonIsDisplayed()
+        {
+            var wait = new WebDriverWait(driver, new TimeSpan(0, 0, 5));
+            wait.Until(condition =>
+            {
+                try
+                {
+                    return UnapproveButton.Displayed;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+        }
+
+    }
+}

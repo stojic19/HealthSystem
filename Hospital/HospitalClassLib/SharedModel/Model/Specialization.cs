22cca48e023074321e2772a78c13d79aebc779d4
jradman12 <jelenaradman12@uns.ac.rs>
2021-12-25 13:57:58 +0100

    feat: specialization as entity

--- Hospital/HospitalClassLib/SharedModel/Model/Specialization.cs
+++ Hospital/HospitalClassLib/SharedModel/Model/Specialization.cs
@@ -1,9 +1,28 @@
-﻿namespace Hospital.SharedModel.Model
+﻿using Microsoft.Extensions.Options;
+
+namespace Hospital.SharedModel.Model
 {
     public class Specialization
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+
+        public Specialization()
+        {
+            
+        }
+        public Specialization(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Validate();
+        }
+
+        private void Validate()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }

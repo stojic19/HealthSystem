9444abd92543d7c194bb2aadc2053d35a8c147f3
AleksandarRadisic <aleksandar.radisic@uns.ac.rs>
2021-12-27 18:21:37 +0100

    feat: Fixed winning offer foreign key

--- Integration/IntegrationClassLib/Database/Migrations/20211227171022_TenderWinningOfferFix.Designer.cs
+++ Integration/IntegrationClassLib/Database/Migrations/20211227171022_TenderWinningOfferFix.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Integration.Database.EfStructures;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Integration.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211227171022_TenderWinningOfferFix")]
+    partial class TenderWinningOfferFix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -197,15 +199,6 @@ namespace Integration.Migrations
                     b.Property<int>("CityId")
                         .HasColumnType("integer");
 
-                    b.Property<bool>("GrpcSupported")
-                        .HasColumnType("boolean");
-                        
-                    b.Property<string>("Description")
-                        .HasColumnType("text");
-
-                    b.Property<string>("ImageName")
-                        .HasColumnType("text");
-                        
                     b.Property<bool>("GrpcSupported")
                         .HasColumnType("boolean");
 

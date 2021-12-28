f979fb6c5448b1eebc52fb20ff2f8529c584a27c
MilanPodunavac <milan.podunavac@uns.ac.rs>
2021-12-28 08:24:03 +0100

    feat: Made model and test

---
+++ Integration/IntegrationClassLib/Shared/Model/Location.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using RestSharp;
+using System.Net.Http.Json;
+using Newtonsoft.Json;
+
+namespace Integration.Shared.Model
+{
+    public class LocationInfo
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Postcode { get; set; }
+        public string Formatted { get; set; }
+    }
+
+    public class LocationResults
+    {
+        public List<LocationInfo> Results { get; set; }
+    }
+
+    public class Location
+    {
+        private static string geoapify_api_key = "5bdc7059b84b4066963d7e6c07e9f3af";
+        private double Latitude { get; }
+
+        private double Longitude { get; }
+
+        public Location(double lat, double lon)
+        {
+            Latitude = lat;
+            Longitude = lon;
+            Validate();
+        }
+
+        private void Validate()
+        {
+            if (Latitude < -90 || Latitude > 90 || Longitude <= -180 || Longitude > 180)
+            {
+                throw new Exception();
+            }
+        }
+
+        public City GetCity()
+        {
+            RestClient client = new RestClient();
+            string targetUrl = "https://api.geoapify.com/v1/geocode/reverse?lat=" + Latitude.ToString().Replace(",", ".") + "&lon=" + Longitude.ToString().Replace(",", ".") +
+                               "&format=json&apiKey=" + geoapify_api_key;
+            RestRequest request = new RestRequest(targetUrl);
+            IRestResponse response = client.Get(request);
+            LocationInfo result = JsonConvert.DeserializeObject<LocationResults>(response.Content).Results.First();
+            return new City
+            {
+                Name = result.City,
+                Country = new Country {Name = result.Country }
+            };
+        }
+    }
+}
---
+++ Integration/IntegrationUnitTests/LocationTests.cs
@@ -0,0 +1,33 @@
+﻿using IntegrationUnitTests.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Integration.Shared.Model;
+using Shouldly;
+using Xunit;
+
+namespace IntegrationUnitTests
+{
+    public class LocationTests : BaseTest
+    {
+        public LocationTests(BaseFixture fixture) : base(fixture)
+        {
+
+        }
+
+        [Fact]
+        public void Get_return_of_city_in_location()
+        {
+            ClearDbContext();
+            Location location = new Location(45.258253, 19.8012564);
+
+            City city = location.GetCity();
+
+            city.ShouldNotBeNull();
+            city.Name.ShouldBe("Novi Sad City");
+            city.Country.Name.ShouldBe("Serbia");
+        }
+    }
+}

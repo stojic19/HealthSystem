95f24d96150fb7c22dc2e9035804fcc880d24adb
Nikola Milosavljevic <73236646+NikolaRA5@users.noreply.github.com>
2022-01-04 14:16:21 +0100

    feat: docker compose prop (#114)
    * feat: proba
    
    * feat: feat: connected db and backend, init.sql yet to do
    
    * feat: proba
    
    * feat: get local init file
    
    * feat: file system sql script
    
    * feat: Fixed port in connection string
    
    * feat: nginx initial setup
    
    * feat: docker compose build succees # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # Author: srdjansukovic <sukovic.ra3.2018@uns.ac.rs> # # interactive rebase in progress; onto 7cda7f8 # Last commands done (8 commands done): # pick c047b89 feat: nginx initial setup # pick 8511e3b feat: docker compose build succees # Next commands to do (4 remaining commands): # pick 019c1f8 feat: docker-compose setup with nginx # pick 84a52e2 feat: starting out docker compose # You are currently rebasing branch 'feature/mili-docker-compose' on '7cda7f8'. # # Changes to be committed: # modified: WebClients/PatientWebClient/angular.json # modified: WebClients/PatientWebClient/src/app/app.module.ts # modified: WebClients/PatientWebClient/src/environments/environment.prod.ts #
    
    * feat: docker-compose setup with nginx
    
    * feat: starting out docker compose
    
    * resolved conflicts
    
    * resolved conflicts
    
    * resolved conflicts
    
    * feat: enabled host detection throught environment variable
    
    * resolved conflicts
    
    * feat: finished docker compose
    
    * feat: fixed hospital startup
    
    * feat: added production proxy
    
    * feat: added proxy replacements for production
    
    * feat: renamed integration api
    
    Co-authored-by: srdjansukovic <sukovic.ra3.2018@uns.ac.rs>
    Co-authored-by: lukamatic <matic.ra16.2018@uns.ac.rs>
    Co-authored-by: andjeladjuric <andjeladjuric@uns.ac.rs>

--- WebClients/IntegrationWebClient/src/app/pharmacy/pharmacy.service.ts
+++ WebClients/IntegrationWebClient/src/app/pharmacy/pharmacy.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from "src/environments/environment";
 
 
 @Injectable()
 export class PharmacyService{
 
-    private _APIUrl = 'http://localhost:5000/api';  //uneti url za http get zahtev!
+    private _APIUrl = environment.baseUrl;  //uneti url za http get zahtev!
     
     constructor(private _httpClient: HttpClient) {}
 
     registerPharmacy(val:any){
-        return this._httpClient.post(this._APIUrl+'/PharmacyCommunication/RegisterPharmacy',val);
+        return this._httpClient.post(this._APIUrl+'api/PharmacyCommunication/RegisterPharmacy',val);
     }
 
     getPharmacies(): Observable<any[]>{
-        return this._httpClient.get<any[]>(this._APIUrl+'/Pharmacy/GetPharmacies');
+        return this._httpClient.get<any[]>(this._APIUrl+'api/Pharmacy/GetPharmacies');
     }
 }
--- WebClients/ManagerWebClient/src/app/services/benefits.service.ts
+++ WebClients/ManagerWebClient/src/app/services/benefits.service.ts
@@ -1,26 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BenefitsService {
 
-  private _APIUrl = 'http://localhost:5000/api';  //uneti url za http get zahtev!
+  private _APIUrl = `${environment.baseIntegrationUrl}`;  //uneti url za http get zahtev!
     
   constructor(private _httpClient: HttpClient) {}
 
   getVisibleBenefits(): Observable<any[]>{
-    return this._httpClient.get<any[]>(this._APIUrl+'/Benefit/GetVisibleBenefits');
+    return this._httpClient.get<any[]>(this._APIUrl+'api/Benefit/GetVisibleBenefits');
   }
 
   getBenefitById(id: number): Observable<any[]>{
-    return this._httpClient.get<any[]>(this._APIUrl+`/Benefit/GetBenefitById/${id}`);
+    return this._httpClient.get<any[]>(this._APIUrl+`api/Benefit/GetBenefitById/${id}`);
   }
 
   publishBenefit(id: any){
-    return this._httpClient.post(this._APIUrl+'/Benefit/PublishBenefit', id);
+    return this._httpClient.post(this._APIUrl+'api/Benefit/PublishBenefit', id);
   }
 
   hideBenefit(id: any){

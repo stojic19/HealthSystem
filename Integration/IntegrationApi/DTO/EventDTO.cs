5b05137abdd6b228a2e6c6bd434f346cd1bfc230
NikolaRA5 <milosavljevic.ra5.2018@uns.ac.rs>
2022-01-05 21:16:51 +0100

    feat: Finished event storing model and backend for integration app

---
+++ Integration/IntegrationApi/DTO/EventDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegrationApi.DTO
+{
+    public class EventDTO
+    {
+        public string StateData { get; set; }
+        public int UserId { get; set; }
+    }
+}

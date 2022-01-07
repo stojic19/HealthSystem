698eef22e051dc31b382fa82b5a29b9108b1fa3d
Nikola Milosavljevic <73236646+NikolaRA5@users.noreply.github.com>
2022-01-07 17:31:41 +0100

    feat:event storing (#116)
    * feat: added event store # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # interactive rebase in progress; onto 0187f87 # Last command done (1 command done): # pick 13a8440 feat: added event store # Next command to do (1 remaining command): # pick c720ba0 feat: checkpoint # You are currently rebasing branch 'feature/event-storing' on '0187f87'. # # Changes to be committed: # new file: Hospital/HospitalApi/Controllers/EventController.cs # new file: Hospital/HospitalApi/DTOs/EventDTO.cs # modified: Hospital/HospitalClassLib/Database/EfStructures/AppDbContext.cs # new file: Hospital/HospitalClassLib/Database/Migrations/20220104151811_EventStoring.Designer.cs # new file: Hospital/HospitalClassLib/Database/Migrations/20220104151811_EventStoring.cs # modified: Hospital/HospitalClassLib/Database/Migrations/AppDbContextModelSnapshot.cs # new file: Hospital/HospitalClassLib/EventStoring/Model/StoredEvent.cs # new file: Hospital/HospitalClassLib/EventStoring/Repository/IStoredEventReadRepository.cs # new file: Hospital/HospitalClassLib/EventStoring/Repository/IStoredEventWriteRepository.cs # new file: Hospital/HospitalClassLib/EventStoring/Repository/Implementation/StoredEventReadRepository.cs # new file: Hospital/HospitalClassLib/EventStoring/Repository/Implementation/StoredEventWriteRepository.cs #
    
    * feat: checkpoint # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # interactive rebase in progress; onto 0187f87 # Last commands done (2 commands done): # pick 13a8440 feat: added event store # pick c720ba0 feat: checkpoint # No commands remaining. # You are currently rebasing branch 'feature/event-storing' on '0187f87'. # # Changes to be committed: # modified: Hospital/HospitalApi/HospitalApi.csproj #
    
    * feat: Implemented event storing model and backend for Hospital app
    
    * feat: Added model for Integration event storing
    
    * feat: namespace fix
    
    * feat: Finished event storing model and backend for integration app

---
+++ Integration/IntegrationClassLib/EventStoring/Repository/Implementation/StoredEventReadRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Integration.Database.EfStructures;
+using Integration.EventStoring.Model;
+using Integration.Shared.Repository.Base;
+
+namespace Integration.EventStoring.Repository.Implementation
+{
+    public class StoredEventReadRepository : ReadBaseRepository<Guid, StoredEvent>, IStoredEventReadRepository
+    {
+        public StoredEventReadRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}

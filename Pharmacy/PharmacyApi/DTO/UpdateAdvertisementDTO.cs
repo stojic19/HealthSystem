23a050f844862b803fd93ecc3a56683a2fe329d2
AleksicNikola00 <aleksic.ra24.2018@uns.ac.rs>
2022-01-08 20:42:44 +0100

    feat: implemented controller/service for Advertisement CRUD

---
+++ Pharmacy/PharmacyApi/DTO/UpdateAdvertisementDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using PharmacyApi.DTO.Base;
+
+namespace PharmacyApi.DTO
+{
+    public class UpdateAdvertisementDTO : BaseCommunicationDTO
+    {
+        public string Title { get; set; }
+        [Required(ErrorMessage = "It is necessary to specify the description of ad!")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "It is necessary to specify the id of the updated ad!")]
+        public int AdvertisementId { get; set; }
+    }
+}

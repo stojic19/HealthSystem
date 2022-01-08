8f009d8c2e78f0222f4cd7a824b80d4e9c7d5e2f
Aleksandar Radišić <73358934+AleksandarRadisic@users.noreply.github.com>
2022-01-08 19:12:56 +0100

    Feature/integration end to end tests (#120)
    * feat: End to end test for registration
    
    * feat: End to end fix Added login
    
    * feat: Update BaseIntegrationController.cs
    
    * feat: Interface inheritance fix
    
    * feat: Shouldly added
    
    * feat: Added BasePage
    
    * feat: Update RegisterPharmacyTest.cs
    
    * feat: Update IntegrationEndToEndTests.csproj

---
+++ Integration/IntegrationEndToEndTests/Base/BaseTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Integration.Database.EfStructures;
+using Integration.Shared.Repository.Base;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+using Cookie = System.Net.Cookie;
+
+namespace IntegrationEndToEndTests.Base
+{
+    public abstract class BaseTest : IClassFixture<BaseFixture>, IDisposable
+    {
+        private readonly BaseFixture _fixture;
+        protected readonly IWebDriver _driver;
+
+        protected BaseTest(BaseFixture fixture)
+        {
+            _fixture = fixture;
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("start-maximized");
+            options.AddArguments("disable-infobars");
+            options.AddArguments("--disable-extensions");
+            options.AddArguments("--disable-gpu");
+            options.AddArguments("--disable-dev-shm-usage");
+            options.AddArguments("--no-sandbox");
+            options.AddArguments("--disable-notifications");
+            _driver = new ChromeDriver(options);
+        }
+
+        protected IUnitOfWork UoW => _fixture.UoW;
+        protected AppDbContext Context => _fixture.Context;
+        public CookieContainer CookieContainer => _fixture.CookieContainer;
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+            _fixture.Dispose();
+        }
+    }
+}

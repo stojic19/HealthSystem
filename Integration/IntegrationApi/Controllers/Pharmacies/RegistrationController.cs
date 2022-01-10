da2734d037361e70b2362c977ee93ae881e4fcec
MilanPodunavac <milan.podunavac@uns.ac.rs>
2022-01-10 18:45:20 +0100

    feat: Registration changed to location

--- Integration/IntegrationApi/Controllers/Pharmacies/RegistrationController.cs
+++ Integration/IntegrationApi/Controllers/Pharmacies/RegistrationController.cs
@@ -46,9 +46,9 @@ namespace IntegrationAPI.Controllers.Pharmacies
             {
                 Name = pharmacyDto.PharmacyName,
                 BaseUrl = pharmacyDto.BaseUrl,
-                StreetName = pharmacyDto.StreetName,
-                StreetNumber = pharmacyDto.StreetNumber,
-                City = new City { PostalCode = pharmacyDto.PostalCode, Name = pharmacyDto.CityName, Country = new Country { Name = pharmacyDto.CountryName } },
+                StreetName = location.GetStreetName(),
+                StreetNumber = location.GetHouseNumber(),
+                City = location.GetCity(),
                 ApiKey = pharmacyDto.ApiKey,
                 GrpcSupported = pharmacyDto.GrpcSupported,
                 Location = new Location(pharmacyDto.Latitude, pharmacyDto.Longitude)

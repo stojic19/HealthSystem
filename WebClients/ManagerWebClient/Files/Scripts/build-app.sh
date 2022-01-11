4419233cbbe41efec73219b8a6e1373b043cd30a
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2022-01-11 05:00:53 +0100

    switched back to apline on managerUi

--- WebClients/ManagerWebClient/Files/Scripts/build-app.sh
+++ WebClients/ManagerWebClient/Files/Scripts/build-app.sh
@@ -3,7 +3,7 @@ HOSPITAL_API_URL=$1
 cd WebClient || exit
 export API_HOST=${HOSPITAL_API_URL}
 
-apt-get --update --no-cache add \
+apk --update --no-cache add \
   gettext=0.20.1-r2 && \
 
 envsubst < environment.ts.template > ./src/environments/environment.ts || exit

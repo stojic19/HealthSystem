7160c751e0c58ddf0dd36041941e6d3affa91989
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2022-01-11 04:47:58 +0100

    fixed apk to apt in manager script

--- WebClients/ManagerWebClient/Files/Scripts/build-app.sh
+++ WebClients/ManagerWebClient/Files/Scripts/build-app.sh
@@ -3,7 +3,7 @@ HOSPITAL_API_URL=$1
 cd WebClient || exit
 export API_HOST=${HOSPITAL_API_URL}
 
-apk --update --no-cache add \
+apt --update --no-cache add \
   gettext=0.20.1-r2 && \
 
 envsubst < environment.ts.template > ./src/environments/environment.ts || exit

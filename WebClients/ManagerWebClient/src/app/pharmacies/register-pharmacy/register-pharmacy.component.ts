f6807b1ab30062c8d5dcd3196a42eb30cb73fa5b
AleksandarRadisic <aleksandar.radisic@uns.ac.rs>
2022-01-13 17:21:56 +0100

    fix: Fixed e2e test for registration, and registration page

--- WebClients/ManagerWebClient/src/app/pharmacies/register-pharmacy/register-pharmacy.component.ts
+++ WebClients/ManagerWebClient/src/app/pharmacies/register-pharmacy/register-pharmacy.component.ts
@@ -11,34 +11,21 @@ export class RegisterPharmacyComponent implements OnInit {
 
   constructor(private _PharmacyService: PharmacyService, private toastr: ToastrService) { }
 
-  name:string="";
-  streetNumber:string="";
-  streetName:string="";
-  cityName:string="";
-  postalCode:string="";
-  country:string="";
   baseUrl:string="";
-  grpcSupported:boolean=false;
 
   ngOnInit(): void {
   }
 
   registerPharmacy(){
-    var val = {name:this.name,
-              streetNumber:this.streetNumber,
-              streetName:this.streetName,
-              city:{
-                name:this.cityName,
-                postalCode:this.postalCode,
-                country:{
-                  name:this.country
-                }
-              },
-              baseUrl:this.baseUrl,
-              grpcSupported:this.grpcSupported
+    if(this.baseUrl === ""){
+      this.toastr.error("Please enter url!");
+      return;
     }
-    this._PharmacyService.registerPharmacy(val).subscribe(res => console.log(res),
-     (error) => this.toastr.error(error.message));
+    var val = {
+              baseUrl:this.baseUrl,
+    }
+    this._PharmacyService.registerPharmacy(val).subscribe(res => this.toastr.success(res.toString()),
+     (error) => this.toastr.error(error.error));
   }
 
 }

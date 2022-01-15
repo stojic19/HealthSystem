f36b94db97660221115f0c76efa5925f7add5d57
momoirox <majablgic505@gmail.com>
2022-01-15 16:19:31 +0100

    feat: Tests not working

--- Hospital/SeleniumTests/CancelAppointmentTests.cs
+++ Hospital/SeleniumTests/CancelAppointmentTests.cs
@@ -18,14 +18,15 @@ using Xunit;
 
 namespace SeleniumTests
 {
+    [Collection("Sequence")]
     public class CancelAppointmentTests : BaseTest, IDisposable
     {
 
         private readonly IWebDriver driver;
         private readonly CancelAppointmentPage cancelAppointmentPage;
-        public readonly string LoginUri = "http://localhost:4200/record";
         private readonly LoginPage loginPage;
      
+
         public CancelAppointmentTests(BaseFixture fixture) : base(fixture)
         {
            
@@ -48,157 +49,33 @@ namespace SeleniumTests
         [Fact]
         public void CancelAppointment()
         {
-
+            RegisterUser("Patient");
+            var events = ArrangeDatabase(false, false);
             InsertCredentials();
             loginPage.EnsureUserIsLogged();      
             cancelAppointmentPage.EnsurePageIsDisplayed();
             cancelAppointmentPage.CancelAppointment();           
             Assert.True(cancelAppointmentPage.ElementNumberChanged());
             Assert.Equal(driver.Url, cancelAppointmentPage.URI);
-
-
+            ClearDatabase(events);
+            DeleteDataFromDataBase();
         }
+
         private void InsertCredentials()
         {
-            loginPage.InsertUsername("andji");
-            loginPage.InsertPassword("Andji1234");
+            loginPage.InsertUsername("testPatientUsername");
+            loginPage.InsertPassword("TestProba123");
             loginPage.Submit();
         }
 
-        private ScheduledEvent AddDataToDatabase(bool isCanceled, bool isDone)
-        {
-            #region Data
-
-            var countryWriteRepo = UoW.GetRepository<ICountryWriteRepository>();
-            var cityWriteRepo = UoW.GetRepository<ICityWriteRepository>();
-            var roomWriteRepo = UoW.GetRepository<IRoomWriteRepository>();
-            var specializationWriteRepo = UoW.GetRepository<ISpecializationWriteRepository>();
-            var doctorWiteRepo = UoW.GetRepository<IDoctorWriteRepository>();
-            var patientWriteRepo = UoW.GetRepository<IPatientWriteRepository>();
-
-            var testCountry = UoW.GetRepository<ICountryReadRepository>().GetAll().Where(x => x.Name == "TestCountry").FirstOrDefault();
-            var testCity = UoW.GetRepository<ICityReadRepository>().GetAll().Where(x => x.Name == "TestCity").FirstOrDefault();
-            var testRoom = UoW.GetRepository<IRoomReadRepository>().GetAll().Where(x => x.Name == "TestRoom").FirstOrDefault();
-            var testSpecialization = UoW.GetRepository<ISpecializationReadRepository>().GetAll().Where(x => x.Name == "TestSpecialization").FirstOrDefault();
-            var testDoctor = UoW.GetRepository<IDoctorReadRepository>().GetAll().Where(x => x.FirstName == "TestDoctor").FirstOrDefault(); 
-            var testPatient = UoW.GetRepository<IPatientReadRepository>().GetAll().Where(x => x.FirstName == "TestPatient").FirstOrDefault();
-
-            if (testCountry == null)
-            {
-                testCountry = new Country()
-                {
-                    Name = "TestCountry"
-                };
-                countryWriteRepo.Add(testCountry);
-            }
-
-            if (testCity == null)
-            {
-                testCity = new City()
-                {
-                    Name = "TestCity",
-                    Country = testCountry,
-                    PostalCode = 00000
-                };
-                cityWriteRepo.Add(testCity);
-            }
-            if (testRoom == null)
-            {
-                testRoom = new Room()
-                {
-                    Name = "TestRoom",
-                    RoomType = RoomType.AppointmentRoom,
-                    FloorNumber = 1,
-                    Description = "TestDescription"
-
-                };
-                roomWriteRepo.Add(testRoom);
-            }
-            if (testSpecialization == null)
-            {
-                testSpecialization = new Specialization()
-                {
-                    Description = "DescriptionSpecialization",
-                    Name = "TestSpecialization"
-                };
-                specializationWriteRepo.Add(testSpecialization);
-            }
-            if (testDoctor == null)
-            {
-                testDoctor = new Doctor()
-                {
-                    FirstName = "TestDoctor",
-                    LastName = "TestDoctorLastName",
-                    MiddleName = "TestDoctorMiddleName",
-                    DateOfBirth = DateTime.Now,
-                    Gender = Gender.Female,
-                    Street = "TestDoctorStreet",
-                    City = testCity,
-                    Room = testRoom,
-                    Shift = new Shift()
-                    {
-                        From = 8,
-                        To = 4,
-                        Name = "prva"
-                    },
-                    Specialization = testSpecialization,
-                    UserName = "testDoctorUsername",
-                    PasswordHash = "AQAAAAEAACcQAAAAEAUFvPAmYz/KKw+6GtOINkZCHX8uCRT8X5nX3/UBzPE1kIVqNXM+Efkye1IkDc6uIg==",
-                    Email = "testDoctor@gmail.com",
-                    EmailConfirmed = true,
-                    PhoneNumber = "testDoctorPhoneNumber",
-                    PhoneNumberConfirmed = false,
-                    TwoFactorEnabled = false,
-                    LockoutEnabled = false,
-                    AccessFailedCount = 0,
-                   
-
-                };
-                doctorWiteRepo.Add(testDoctor);
-            }
-            if (testPatient == null)
-            {
-                MedicalRecord medicalRecord = new MedicalRecord
-                {
-                    Doctor = testDoctor,
-                    Height = 0.0,
-                    Weight = 0.0,
-                    BloodType = BloodType.ABNegative,
-                    JobStatus = JobStatus.Student
-
-                };
-
-                testPatient = new Patient()
-                {
-                    
-                    FirstName = "TestPatient",
-                    MiddleName = "TestPatientMiddleName",
-                    LastName = "TestPatientLastName",
-                    DateOfBirth = DateTime.Now,
-                    Gender = Gender.Female,
-                    Street = "TesPatientStreet",
-                    City = testCity,
-                    IsBlocked = false,
-                    UserName = "testPatientUsername",
-                    PasswordHash = "AQAAAAEAACcQAAAAEAUFvPAmYz/KKw+6GtOINkZCHX8uCRT8X5nX3/UBzPE1kIVqNXM+Efkye1IkDc6uIg==",
-                    Email = "testPatient@gmail.com",
-                    EmailConfirmed = true,
-                    PhoneNumber = "testPatientPhoneNumber",
-                    PhoneNumberConfirmed = false,
-                    TwoFactorEnabled = false,
-                    LockoutEnabled = false,
-                    AccessFailedCount = 0,
-                    MedicalRecord = medicalRecord,
-                    NormalizedUserName = "TESTPATIENTUSERNAME",
-                    SecurityStamp = "XJXBBWC54VRCAR5QA6Z6EGMKD4HIDH6M"
-                };          
-                patientWriteRepo.Add(testPatient);
-            }
-            #endregion
+        private ScheduledEvent ArrangeDatabase(bool isCanceled, bool isDone)
+        {          
+            var testRoom = UoW.GetRepository<IRoomReadRepository>().GetAll().Where(x => x.Name == "TestRoom").FirstOrDefault();     
+            var testDoctor = UoW.GetRepository<IDoctorReadRepository>().GetAll().Where(x => x.UserName == "testDoctorUsername").FirstOrDefault(); 
+            var testPatient = UoW.GetRepository<IPatientReadRepository>().GetAll().Where(x => x.UserName == "testPatientUsername").FirstOrDefault();
 
             ScheduledEvent scheduledEvent = new ScheduledEvent()
             {
-
                 ScheduledEventType = 0,
                 IsCanceled = isCanceled,
                 IsDone = isDone,
@@ -206,7 +83,8 @@ namespace SeleniumTests
                 EndDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.AddDays(3).Day),
                 Patient = testPatient,
                 Doctor = testDoctor,
-                Room = testRoom
+                Room = testRoom,
+                
 
             };
 
@@ -214,7 +92,7 @@ namespace SeleniumTests
             return scheduledEvent;
         }
 
-        private void DeleteDataFromDataBase(ScheduledEvent events)
+        private void ClearDatabase(ScheduledEvent events)
         {
             UoW.GetRepository<IScheduledEventWriteRepository>().Delete(events, true);
         }

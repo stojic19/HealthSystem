6b7f70a6d176bfca4fb9271a1f3818e8907c3691
momoirox <majablgic505@gmail.com>
2022-01-19 23:37:17 +0100

    feat: unit test

--- Hospital/HospitalUnitTests/CancelingAppointmentTests.cs
+++ Hospital/HospitalUnitTests/CancelingAppointmentTests.cs
@@ -1,91 +1,77 @@
-﻿//using Hospital.MedicalRecords.Model;
-//using Hospital.RoomsAndEquipment.Model;
-//using Hospital.Schedule.Model;
-//using Hospital.Schedule.Service;
-//using Hospital.SharedModel.Model;
-//using HospitalUnitTests.Base;
-//using Shouldly;
-//using System;
-//using Xunit;
+﻿using Hospital.MedicalRecords.Model;
+using Hospital.RoomsAndEquipment.Model;
+using Hospital.Schedule.Model;
+using Hospital.Schedule.Service;
+using Hospital.SharedModel.Model;
+using HospitalUnitTests.Base;
+using Shouldly;
+using System;
+using Xunit;
 
-//namespace HospitalUnitTests
-//{
-//    public class CancelingAppointmentTests : BaseTest
-//    {
-//        public CancelingAppointmentTests(BaseFixture fixture) : base(fixture)
-//        {
-//        }
-//        [Fact]
-//        public void Appointment_should_be_cancelled()
-//        {
-//            ClearDbContext();
-//            createDbContext(isCanceled: false, isDone: false);
-//            ScheduledEventService scheduledEventsService = new(UoW);
+namespace HospitalUnitTests
+{
+    public class CancelingAppointmentTests : BaseTest
+    {
+        public CancelingAppointmentTests(BaseFixture fixture) : base(fixture)
+        {
+        }
+        [Fact]
+        public void Appointment_should_be_cancelled()
+        {
+            ClearDbContext();
+            createDbContext(isCanceled: false, isDone: false);
+            ScheduledEventService scheduledEventsService = new(UoW);
 
-//            ScheduledEvent scheduled = scheduledEventsService.GetScheduledEvent(1);
-//            scheduledEventsService.CancelAppointment(1);
-//            scheduledEventsService.GetCanceledUserEvents("testUser").Count.ShouldNotBe(0);
-//        }
-//        [Fact]
-//        public void Appointment_should_not_be_cancelled()
-//        {
-//            ClearDbContext();
-//            createDbContext(isCanceled: false, isDone: false);
-//            ScheduledEventService scheduledEventsService = new(UoW);
-//            updateEventTime(scheduledEventsService);
-//            scheduledEventsService.CancelAppointment(1);
-//            scheduledEventsService.GetCanceledUserEvents("testUser").Count.ShouldBe(0);
-//        }
+            ScheduledEvent scheduled = scheduledEventsService.GetScheduledEvent(1);
+            scheduledEventsService.CancelAppointment(1);
+            scheduledEventsService.GetCanceledUserEvents("testUser").Count.ShouldNotBe(0);
+        }
+        [Fact]
+        public void Appointment_should_not_be_cancelled()
+        {
+            ClearDbContext();
+            createDbContext(isCanceled: false, isDone: false);
+            ScheduledEventService scheduledEventsService = new(UoW);
+            updateEventTime(scheduledEventsService);
+            scheduledEventsService.CancelAppointment(1);
+            scheduledEventsService.GetCanceledUserEvents("testUser").Count.ShouldBe(0);
+        }
 
-//        private void updateEventTime(ScheduledEventService scheduledEventsService)
-//        {
-//            ScheduledEvent scheduled = scheduledEventsService.GetScheduledEvent(1);
-//            scheduled.StartDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.AddDays(-2).Day);
-//            scheduled.EndDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.AddDays(-2).Day);
-//            Context.SaveChanges();
-//        }
+        private void updateEventTime(ScheduledEventService scheduledEventsService)
+        {
+            ScheduledEvent scheduled = scheduledEventsService.GetScheduledEvent(1);
+            scheduled.UpdateTime(scheduled.StartDate.AddDays(-3), scheduled.EndDate.AddDays(-3));
+            Context.SaveChanges();
+        }
 
-//        [Fact]
-//        public void Finished_appointment_should_not_be_cancelled()
-//        {
-//            ClearDbContext();
-//            createDbContext(isCanceled: false, isDone: true);
-//            ScheduledEventService scheduledEventsService = new(UoW);
-//            scheduledEventsService.CancelAppointment(1);
-//            scheduledEventsService.GetCanceledUserEvents("testUser").Count.ShouldBe(0);
-//        }
+        [Fact]
+        public void Finished_appointment_should_not_be_cancelled()
+        {
+            ClearDbContext();
+            createDbContext(isCanceled: false, isDone: true);
+            ScheduledEventService scheduledEventsService = new(UoW);
+            scheduledEventsService.CancelAppointment(1);
+            scheduledEventsService.GetCanceledUserEvents("testUser").Count.ShouldBe(0);
+        }
 
-//        private void createDbContext(bool isCanceled, bool isDone)
-//        {
-//            Context.Patients.Add(new Patient()
-//            {
-//                Id = 10,
-//                UserName = "testUser"
-//            });
-//            Context.Doctors.Add(new Doctor()
-//            {
-//                Id = 20
-//            });
-//            Context.Rooms.Add(new Room()
-//            {
-//                Id = 10
-//            });
+        private ScheduledEvent createDbContext(bool isCanceled, bool isDone)
+        {
+            Patient testPatient = new Patient();
+            
+            Context.Patients.Add(testPatient);
+            Doctor testDoctor = new Doctor();
+            Context.Doctors.Add(testDoctor);
+            Context.Rooms.Add(new Room()
+            {
+                Id = 10
+            });
+            ScheduledEvent scheduledEvent = new ScheduledEvent(0, isCanceled, isDone, new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.AddDays(3).Day), new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.AddDays(3).Day),
+                       new DateTime(), testPatient.Id, testDoctor.Id, testDoctor);
 
-//            Context.ScheduledEvents.Add(new ScheduledEvent()
-//            {
-//                Id = 1,
-//                ScheduledEventType = 0,
-//                IsCanceled = isCanceled,
-//                IsDone = isDone,
-//                StartDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.AddDays(3).Day),
-//                EndDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.AddDays(3).Day),
-//                PatientId = 10,
-//                DoctorId = 20,
-//                RoomId = 10
-//            }
-//            );
+            Context.ScheduledEvents.Add(scheduledEvent);
 
-//            Context.SaveChanges();
-//        }
-//    }
-//}
+            Context.SaveChanges();
+            return scheduledEvent;
+        }
+    }
+}

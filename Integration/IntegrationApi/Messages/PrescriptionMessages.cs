519af7bb56219e566ed5619ccfc42423db453b9d
MilanPodunavac <milan.podunavac@uns.ac.rs>
2022-01-23 18:34:03 +0100

    fix: Prescription refactoring and messaging

---
+++ Integration/IntegrationApi/Messages/PrescriptionMessages.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegrationApi.Messages
+{
+    public class PrescriptionController
+    {
+        public static readonly string NoMedicineInAnyPharmacy = "No pharmacy has the medicine";
+    }
+}

ac02e4203bbf3a89309cc3b823efb4b236b15f81
MilanPodunavac <milan.podunavac@uns.ac.rs>
2022-01-23 17:11:09 +0100

    fix: Benefit refactoring and messaging

---
+++ Integration/IntegrationApi/Messages/BenefitMessages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegrationApi.Messages
+{
+    public class BenefitMessages
+    {
+        public static readonly string WrongId = "Benefit with that ID not found";
+        public static readonly string AlreadyPublished = "Benefit is already published";
+        public static readonly string CannotPublish = "Error, could not publish benefit";
+        public static readonly string ConfirmPublish = "Benefit published";
+        public static readonly string AlreadyHidden = "Benefit is already hidden";
+        public static readonly string CannotHide = "Error, could not hide benefit";
+        public static readonly string ConfirmHide = "Benefit hidden";
+    }
+}

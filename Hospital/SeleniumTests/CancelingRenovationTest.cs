95edfe6f8ec6de7deac42e60141494ecc3ec1835
andjeladjuric <andjeladjuric@uns.ac.rs>
2022-01-24 02:02:07 +0100

    feat: created test for cancelling renovations

---
+++ Hospital/SeleniumTests/CancelingRenovationTest.cs
@@ -0,0 +1,128 @@
+﻿using Hospital.MedicalRecords.Repository;
+using Hospital.RoomsAndEquipment.Model;
+using Hospital.RoomsAndEquipment.Repository;
+using Hospital.SharedModel.Model.Enumerations;
+using Hospital.SharedModel.Repository;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumTests.Base;
+using SeleniumTests.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SeleniumTests
+{
+    [Collection("Sequence")]
+    public class CancelingRenovationTest : BaseTest, IDisposable
+    {
+        private readonly IWebDriver driver;
+        private readonly CancelRenovationPage cancelRenovationPage;
+        private readonly LoginPage loginPage;
+
+        public CancelingRenovationTest(BaseFixture fixture) : base(fixture)
+        {
+
+            var options = new ChromeOptions();
+            options.AddArguments("start-maximized");
+            options.AddArguments("disable-infobars");
+            options.AddArguments("--disable-extensions");
+            options.AddArguments("--disable-gpu");
+            options.AddArguments("--disable-dev-shm-usage");
+            options.AddArguments("--no-sandbox");
+            options.AddArguments("--disable-notifications");
+
+            driver = new ChromeDriver(options);
+            loginPage = new LoginPage(driver);
+            loginPage.Navigate();
+            loginPage.EnsureLoginFormForUserIsDisplayed();
+            cancelRenovationPage = new CancelRenovationPage(driver);
+        }
+
+        [Fact]
+        public void CancelRenovation()
+        { 
+            RegisterUser("Manager");
+
+            var events = ArrangeDatabase();
+            var testRoom = UoW.GetRepository<IRoomReadRepository>()
+                .GetAll()
+                .Where(x => x.Name == "TestCancelRoom")
+                .FirstOrDefault();
+
+            InsertCredentials();
+            loginPage.EnsureAdminIsLoggedIn();
+            cancelRenovationPage.Navigate(testRoom.Id);
+            cancelRenovationPage.EnsurePageIsDisplayed();
+            var initialRowsCount = cancelRenovationPage.EventsCount();
+            cancelRenovationPage.CancelEvent();
+            Assert.True(cancelRenovationPage.EventsNumberChanged(initialRowsCount));
+            Assert.Equal(driver.Url, "http://localhost:4200/schedule/" + testRoom.Id);
+            ClearDatabase();
+            DeleteDataFromDataBase();
+        }
+
+        private void InsertCredentials()
+        {
+            loginPage.InsertUsername("testLogInManager");
+            loginPage.InsertPassword("111111aA");
+            loginPage.Submit();
+        }
+
+        private RoomRenovationEvent ArrangeDatabase()
+        {
+            var testRoom = UoW.GetRepository<IRoomReadRepository>().GetAll().Where(x => x.Name == "TestCancelRoom").FirstOrDefault();
+            
+            if (testRoom == null)
+            {
+                testRoom = new Room()
+                {
+                    Name = "TestCancelRoom",
+                    FloorNumber = 1,
+                    BuildingName = "Building 1",
+                    RoomType = RoomType.AppointmentRoom,
+                    RoomPosition = new Hospital.GraphicalEditor.Model.RoomPosition(155, 155, 155, 155)
+                };
+                UoW.GetRepository<IRoomWriteRepository>().Add(testRoom);
+
+            }
+
+            RoomRenovationEvent scheduledEvent = new RoomRenovationEvent()
+            {
+                StartDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.AddDays(3).Day),
+                EndDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.AddDays(3).Day),
+                RoomId = testRoom.Id,
+                IsMerge = false,
+                FirstRoomType = RoomType.AppointmentRoom,
+                FirstRoomDescription = "First split room",
+                FirstRoomName = "First room",
+                SecondRoomDescription = "Second split room",
+                SecondRoomName = "Second room",
+                SecondRoomType = RoomType.AppointmentRoom
+            };
+
+            UoW.GetRepository<IRoomRenovationEventWriteRepository>().Add(scheduledEvent);
+            return scheduledEvent;
+        }
+
+        private void ClearDatabase()
+        {
+            Room room = UoW.GetRepository<IRoomReadRepository>()
+                .GetAll()
+                .Where(x => x.Name == "TestCancelRoom")
+                .FirstOrDefault();
+            
+            if (room != null)
+                UoW.GetRepository<IRoomWriteRepository>().Delete(room);
+        }
+
+        public void Dispose()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+    }
+}
---
+++ Hospital/SeleniumTests/Pages/CancelRenovationPage.cs
@@ -0,0 +1,167 @@
+﻿using Hospital.RoomsAndEquipment.Repository;
+using Hospital.SharedModel.Repository.Base;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumTests.Base;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumTests.Pages
+{
+    public class CancelRenovationPage : BasePage
+    {
+        private readonly IWebDriver _driver;
+
+        public readonly string URI;
+
+        private int InitialEventCount;
+        private IWebElement CancelButton => _driver.FindElement(By.Id("cancelButton"));
+        private IWebElement ConfirmButton => _driver.FindElement(By.Id("confirm"));
+        private IWebElement Table => _driver.FindElement(By.Id("eventTable"));
+        private ReadOnlyCollection<IWebElement> Rows => _driver.FindElements(By.XPath("//table[@id='eventTable']/tbody/tr"));
+
+
+        public CancelRenovationPage(IWebDriver driver) : base(driver)
+        {
+            this._driver = driver;
+            this.URI = base._baseUrlMan + "/schedule/";
+        }
+
+        public void Navigate(int testRoomId) => _driver.Navigate().GoToUrl(URI + testRoomId);
+        public void CancelEvent()
+        {
+            CancelButton.Click();
+            EnsureConfirmDialogIsOpened();
+            ConfirmButton.Click();
+            Thread.Sleep(1000);
+            EnsurePageIsDisplayed();
+        }
+
+        internal void EnsurePageIsDisplayed()
+        {
+            var wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 30));
+            wait.Until(condition =>
+            {
+                try
+                {
+                    return Table.Displayed;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+        }
+
+        internal bool CancelButtonDisplayed()
+        {
+            return CancelButton.Displayed;
+        }
+
+        internal bool ConfirmButtonDisplayed()
+        {
+            return ConfirmButton.Displayed;
+        }
+
+        internal bool IsDialogDisplayed()
+        {
+            try
+            {
+                return _driver.FindElement(By.TagName("app-confirm-dialog")).Displayed;
+            }
+            catch (StaleElementReferenceException)
+            {
+                return false;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        public void EnsureConfirmButtonIsDisplayed()
+        {
+            var wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 5));
+            wait.Until(condition =>
+            {
+                try
+                {
+                    return ConfirmButton.Displayed;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+        }
+
+        public void EnsureCancelButtonIsDisplayed()
+        {
+            var wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 5));
+            wait.Until(condition =>
+            {
+                try
+                {
+                    return CancelButton.Displayed;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+        }
+        public void EnsureConfirmDialogIsOpened()
+        {
+            var wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 5));
+            wait.Until(condition =>
+            {
+                try
+                {
+                    return _driver.FindElement(By.TagName("app-confirm-dialog")).Displayed;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+        }
+
+        public bool EventsNumberChanged(int initialRowsCount)
+        {
+            if (initialRowsCount - EventsCount() == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        internal int EventsCount()
+        {
+            return Rows.Count;
+        }
+    }
+}

a2b2c8ef439addce64b0b03eb87d338637c5f7b2
bobabnoba <bozanaruljic""@@uns.ac.rs>
2022-01-25 02:48:30 +0100

    feat: database cleanup in tests added

--- Hospital/HospitalIntegrationTests/PrescriptionTests.cs
+++ Hospital/HospitalIntegrationTests/PrescriptionTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
@@ -6,6 +7,7 @@ using AutoMapper;
 using Hospital.MedicalRecords.Model;
 using Hospital.MedicalRecords.Repository;
 using Hospital.Schedule.Model;
+using Hospital.Schedule.Repository;
 using Hospital.SharedModel.Repository;
 using HospitalApi.Controllers;
 using HospitalApi.DTOs;
@@ -79,7 +81,7 @@ namespace HospitalIntegrationTests
         {
             RegisterAndLogin("Patient");
             
-            var scheduledEventId = PrepareDatabase();
+            var scheduledEventId = PrepareDatabase(1);
 
             var response = await PatientClient.GetAsync(BaseUrl +
                                                          "api/Prescription/GetPrescriptionForScheduledEvent?scheduledEventId=" +
@@ -87,43 +89,96 @@ namespace HospitalIntegrationTests
 
             response.StatusCode.ShouldBe(HttpStatusCode.OK);
 
-            var prescriptions = UoW.GetRepository<IPatientReadRepository>().GetAll()
+            var patient = UoW.GetRepository<IPatientReadRepository>().GetAll()
                 .Include(p => p.MedicalRecord)
-                .ThenInclude(mr => mr.Prescriptions);
+                .ThenInclude(mr => mr.Prescriptions).FirstOrDefault(p => p.UserName == "testPatientUsername");
 
-            prescriptions.Count().ShouldBe(1);
-
-            //delete data from database
+            patient.MedicalRecord.Prescriptions.Count().ShouldBe(1);
+            patient.MedicalRecord.Prescriptions.First().EndDate.ShouldBe(new DateTime(2022, 01, 31));
+            DatabaseCleanUp();
+            DeleteDataFromDataBase();
         }
 
-        private int  PrepareDatabase()
+        [Fact]
+        public async Task Should_return_no_content()
+        {
+            RegisterAndLogin("Patient");
+
+            var scheduledEventId = PrepareDatabase(2);
+
+            var response = await PatientClient.GetAsync(BaseUrl +
+                                                        "api/Prescription/GetPrescriptionForScheduledEvent?scheduledEventId=" +
+                                                        scheduledEventId + "&patientUsername=" + "testPatientUsername");
+
+            response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+
+            var patient = UoW.GetRepository<IPatientReadRepository>().GetAll()
+                .Include(p => p.MedicalRecord)
+                .ThenInclude(mr => mr.Prescriptions).FirstOrDefault(p => p.UserName == "testPatientUsername");
+
+            patient.MedicalRecord.Prescriptions.Count().ShouldBe(0);
+            DatabaseCleanUp();
+            DeleteDataFromDataBase();
+        }
+
+        private int PrepareDatabase(int testNumber)
         {
             var patient = UoW.GetRepository<IPatientReadRepository>()
                 .GetAll()
                 .Include(p => p.MedicalRecord)
-                .ThenInclude(mr => mr.Prescriptions)
                 .FirstOrDefault(p => p.UserName == "testPatientUsername");
 
             var doctor = UoW.GetRepository<IDoctorReadRepository>()
-                .GetAll().FirstOrDefault(d => d.UserName == "testDoctorUsername");
+                .GetAll().Include(d => d.Room).FirstOrDefault(d => d.UserName == "testDoctorUsername");
 
-            var scheduledEvent = new ScheduledEvent(0, false, true, new DateTime(2022, 01, 24, 13, 00, 00), new DateTime(2022, 01, 24, 13, 30, 00), new DateTime(), patient.Id, doctor.Id, doctor);
+            var scheduledEvent = new ScheduledEvent(0, false, true, new DateTime(2022, 01, 24, 13, 00, 00), new DateTime(2022, 01, 24, 13, 30, 00), 
+                new DateTime(),  patient.Id, doctor.Id, doctor);
 
-            var prescriptions =  patient.MedicalRecord.Prescriptions.ToList();
-            prescriptions.Add(new Prescription()
+            var secondScheduledEvent = new ScheduledEvent(0, false, true, new DateTime(2022, 01, 25, 13, 00, 00), new DateTime(2022, 01, 25, 13, 30, 00),
+                new DateTime(), patient.Id, doctor.Id, doctor);
+
+            UoW.GetRepository<IScheduledEventWriteRepository>().Add(scheduledEvent);
+            UoW.GetRepository<IScheduledEventWriteRepository>().Add(secondScheduledEvent);
+
+            if (testNumber == 1)
             {
-                Patient = patient,
-                IssuedDate = DateTime.Now,
-                StartDate = new DateTime(2022, 01, 27),
-                EndDate = new DateTime(2022, 01, 31),
-                Medication = new Medication()
+                var prescription = new Prescription()
                 {
-                    Name = "Paracetamol"
-                }, 
-                ScheduledEvent = scheduledEvent
-            });
-            return scheduledEvent.Id;
+                    Patient = patient,
+                    IssuedDate = DateTime.Now,
+                    StartDate = new DateTime(2022, 01, 27),
+                    EndDate = new DateTime(2022, 01, 31),
+                    Medication = new Medication()
+                    {
+                        Name = "Paracetamol"
+                    },
+                    ScheduledEvent = scheduledEvent
+                };
+
+                patient.AddPrescription(prescription);
+                UoW.GetRepository<IPatientWriteRepository>().Update(patient);
+            }
+            return testNumber == 1 ? scheduledEvent.Id : secondScheduledEvent.Id;
         }
+
+        private void DatabaseCleanUp()
+        {
             
+            var se1 = UoW.GetRepository<IScheduledEventReadRepository>().GetAll()
+                .FirstOrDefault((se => DateTime.Compare(se.EndDate, new DateTime(2022, 01, 24, 13, 30, 00)) == 0));
+            UoW.GetRepository<IScheduledEventWriteRepository>().Delete(se1);
+            
+            var se2 = UoW.GetRepository<IScheduledEventReadRepository>().GetAll().FirstOrDefault((se => DateTime.Compare(se.EndDate, new DateTime(2022, 01, 25, 13, 30, 00)) == 0));
+            UoW.GetRepository<IScheduledEventWriteRepository>().Delete(se2);
+            
+            var prescriptions = UoW.GetRepository<IPrescriptionReadRepository>().GetAll().Include(p => p.Patient)
+                .Where(pr => pr.Patient.UserName == "testPatientUsername");
+            foreach (var pr in prescriptions)
+            {
+                UoW.GetRepository<IPrescriptionWriteRepository>().Delete(pr);
+            }
+        }
+
+
     }
 }
